cb05d93c9997833fcfc941e02c622e2e1ab4ddb5
Ruiyue Wang <phynew@hotmail.com>
2020-12-26 11:57:57 -0500

    Create 535. Is Prime Number.java

---
+++ 535. Is Prime Number.java
@@ -0,0 +1,16 @@
+Easy
+
+Determine whether an integer n is a prime number. Assumption: n is >= 2.
+
+public class Solution {
+  public boolean isPrime(int n) {
+    for (int i = 2; i * i < n; ++i ) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+Solution
