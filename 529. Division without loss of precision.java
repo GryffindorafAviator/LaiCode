daeabb6209b2f8eff04b4036f2ddcfbcda2ac516
Ruiyue Wang <phynew@hotmail.com>
2020-12-26 12:23:33 -0500

    Create 529. Division without loss of precision.java

---
+++ 529. Division without loss of precision.java
@@ -0,0 +1,13 @@
+Easy
+Given two integer a and b, calculate a divide by b but maintain the precision.
+
+class Solution {
+	public double divide() {
+		int a = 3;
+		int b = 7;	
+		double c = (double) a / b; 
+    
+		return c;
+	}
+}
+
