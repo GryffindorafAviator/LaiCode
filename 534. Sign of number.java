e236f7754226041b6104a71b26098d4aad0ad645
Ruiyue Wang <phynew@hotmail.com>
2020-12-26 11:52:21 -0500

    Create 534. Sign of number.java

---
+++ 534. Sign of number.java
@@ -0,0 +1,15 @@
+Easy
+Return 1 if the input integer is positive, -1 if the input integer is negative, 0 if the input integer is zero.
+
+public class Solution {
+  public int sign(int x) {
+    return x == 0 ? 0 : (x > 0 ? 1 : -1);
+  }
+}
+
+Solution
+public class Solution {
+  public int sign(int x) {
+    if (x > 0)
+  }
+}
