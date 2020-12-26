ec4e294536582ba73ceb9d73327dbdfba03fa993
Ruiyue Wang <phynew@hotmail.com>
2020-12-26 12:11:25 -0500

    Create 536. Calculate Sum of Squares.java

---
+++ 536. Calculate Sum of Squares.java
@@ -0,0 +1,19 @@
+Easy
+Calculate the sum of all square numbers between 1 and n (inclusive).
+
+Example:
+1, n = 3. Return 1;
+2, n = 10. Return 14 (1 + 4 + 9);
+
+public class Solution {
+  public int sumOfSquares(int n) {
+    int i = 1;
+    int ans = 0;
+
+    while (i * i <= n) {
+      ans += i * i;
+      ++i;
+    }
+    return ans;
+  }
+}
