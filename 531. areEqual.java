270911e2fca6a538a9b3c336aa21975ff57a5e37
Ruiyue Wang <phynew@hotmail.com>
2021-01-05 17:56:15 -0500

    Create 531. areEqual.java

---
+++ 531. areEqual.java
@@ -0,0 +1,10 @@
+Easy
+Check if the given number x and y are equal.
+
+public class Solution {
+  public boolean areEqual(int x, int y) {
+    boolean b = x == y ? true : false; 
+    return b;
+  }
+}
+
