239a0b47dfc92734721a805f25a498e395a20de4
Ruiyue Wang <phynew@hotmail.com>
2021-01-05 17:12:53 -0500

    Create 539. Find Minimum.java

---
+++ 539. Find Minimum.java
@@ -0,0 +1,26 @@
+Easy
+Given an array, return its minimum value
+
+Example:
+array = [7, 3, 2], return 2
+
+Note: if the array is null or empty, return 0.
+
+public class Solution {
+  public int min(int[] array) {
+    if (array == null || array.length == 0) {
+      return 0;
+    }
+
+    int min = array[0];
+
+    for (int i = 1; i < array.length; ++i) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    } 
+
+    return min;
+  }
+}
+
