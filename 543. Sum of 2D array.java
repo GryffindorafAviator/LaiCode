56d37f065e91b223e6f69c4afe206af4b9cad935
Ruiyue Wang <phynew@hotmail.com>
2021-01-05 17:50:08 -0500

    Update 543. Sum of 2D array.java

--- 543. Sum of 2D array.java
+++ 543. Sum of 2D array.java
@@ -1,11 +1,11 @@
-Easy
-Given a two-dimensional array, calculate the sum of all its elements
+// Easy
+// Given a two-dimensional array, calculate the sum of all its elements
 
-Example:
-array = {{ 2, 3}, {4, 5}, {1, 0}}
-Return 15
+// Example:
+// array = {{ 2, 3}, {4, 5}, {1, 0}}
+// Return 15
 
-Assumption: The 2D array is not null or empty. The 2D array is rectangular shape.
+// Assumption: The 2D array is not null or empty. The 2D array is rectangular shape.
 
 public class Solution {
   public int sum(int[][] array) {
