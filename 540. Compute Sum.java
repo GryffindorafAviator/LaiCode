f7344e2e215d8684558efd27a37fe5422ea67607
Ruiyue Wang <phynew@hotmail.com>
2021-01-05 17:20:33 -0500

    Create 540. Compute Sum.java

---
+++ 540. Compute Sum.java
@@ -0,0 +1,24 @@
+Easy
+Given an array, return sum of all elements.
+
+Example:
+
+array = [7, 3, 2], return 12
+
+Note: if the array is null or empty, return 0.
+
+public class Solution {
+  public int sum(int[] array) {
+    if (array == null || array.length == 0) {
+      return 0;
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < array.length; ++i) {
+      sum += array[i];
+    }
+
+    return sum;
+  }
+}
