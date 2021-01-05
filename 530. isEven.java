1aa7e4a12d7deab09ddd36d5aa62d1f72e3d91ba
Ruiyue Wang <phynew@hotmail.com>
2021-01-05 17:51:59 -0500

    Create 530. isEven.java

---
+++ 530. isEven.java
@@ -0,0 +1,10 @@
+Easy
+Check if the number x is even, return true for even numbers.
+
+public class Solution {
+  public boolean isEven(int x) {
+    boolean b = x % 2 == 0 ? true : false; //complete the expression
+    return b;
+    // Write your solution here
+  }
+}
