a3eada5e24da1513cfd2e3e6bd13afef3c816bb9
Ruiyue Wang <phynew@hotmail.com>
2021-01-05 17:58:04 -0500

    Create 532. areRoughlyEqual.java

---
+++ 532. areRoughlyEqual.java
@@ -0,0 +1,10 @@
+Easy
+Check if the given number x and y are roughly equal, the allowed error is lower than 0.0001.
+
+public class Solution {
+  public boolean areEqual(double x, double y) {
+    boolean b = Math.abs(x - y) < 0.0001 ? true : false;
+    return b;
+  }
+}
+
