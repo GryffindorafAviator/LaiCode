1482c91007a4815fa9b92e5fe5ab92ccb7e11531
Ruiyue Wang <phynew@hotmail.com>
2021-01-05 17:32:44 -0500

    Create 542. Reverse an array.java

---
+++ 542. Reverse an array.java
@@ -0,0 +1,18 @@
+Easy
+Given an array, reverse all its elements.
+
+Example:
+array = [2, 3, 5, 7, 11]
+result: [11, 7, 5, 3, 2]
+
+Assumption: The array is not null or empty.
+
+public class Solution {
+  public void reverse(int[] array) {
+    for (int i = 0, j = array.length - 1; i < j; ++i, --j) {
+      int temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+}
