5fd8db3b5f94d2493f60eb80464c235ab73f83fe
Ruiyue Wang <phynew@hotmail.com>
2021-01-08 15:20:46 -0500

    Update 544. Design an accumulator.java

--- 544. Design an accumulator.java
+++ 544. Design an accumulator.java
@@ -1,5 +1,5 @@
-Easy
-Design an accumulator, which can take a new integer using function “add”, and can return the sum of all taken integers up to now using function “sum”.
+// Easy
+// Design an accumulator, which can take a new integer using function “add”, and can return the sum of all taken integers up to now using function “sum”.
 
 class Accumulator {
   int ans = 0;
