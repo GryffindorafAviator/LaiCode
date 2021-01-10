21b69023dbd7f8d0b8056b098d428f264019559f
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:24:13 -0500

    Create 742. [Debug] Compute Sum.java

---
+++ 742. [Debug] Compute Sum.java
@@ -0,0 +1,20 @@
+Debug
+Given an array, return the sum of all elements.
+
+Example:
+array = [7, 3, 2], return 12
+
+Note: if the array is null or empty, return -1.
+
+public class Solution {
+  public int sum(int[] array) {
+    if (array == null || array.length == 0) {
+      return -1;
+    }
+    int s = 0;
+    for (int i = 0; i < array.length; i++) {
+      s += array[i];
+    }
+    return s;
+  }
+}
