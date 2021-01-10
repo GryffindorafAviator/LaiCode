1cc223552e492548073819d68aef543d0fe7b28e
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:37:25 -0500

    Create 733. [Debug] Design an accumulator.java

---
+++ 733. [Debug] Design an accumulator.java
@@ -0,0 +1,14 @@
+Debug
+There are bugs in the given code, please fix them.
+
+Design an accumulator, which can take a new integer using function “add”, and can return the sum of all taken integers up to now using function “sum”.
+
+class Accumulator {
+  int s = 0;
+  public void add(int x) {
+    s += x;
+  }
+  public int sum() {
+    return s;
+  }
+}
