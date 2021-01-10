4837a1c613f73ec9c11f54d7824d0f074dd5d2c9
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:14:55 -0500

    Create 741. [Debug] Calculate the factorial of n.java

---
+++ 741. [Debug] Calculate the factorial of n.java
@@ -0,0 +1,19 @@
+Debug
+There are bugs in the given code, please fix them.
+
+Calculate the factorial of n (n > 1).
+
+Example:
+n = 3, return 6
+n = 4, return 24
+n = 5, return 120
+
+public class Solution {
+  public int factorial(int n) {
+    int res = 1;
+    for (int i = 1; i <= n; i++) {
+      res *= i;
+    }
+    return res;
+  }
+}
