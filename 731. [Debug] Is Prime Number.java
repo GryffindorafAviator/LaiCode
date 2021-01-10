bf37534c2e2f07f9df4f239b64e3c6e11e872542
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:31:51 -0500

    Update 731. [Debug] Is Prime Number.java

--- 731. [Debug] Is Prime Number.java
+++ 731. [Debug] Is Prime Number.java
@@ -1,7 +1,7 @@
-Debug
-There are bugs in the given code, please fix them.
+// Debug
+// There are bugs in the given code, please fix them.
 
-Determine whether an integer n is a prime number. Assumption: n is >= 2.
+// Determine whether an integer n is a prime number. Assumption: n is >= 2.
 
 public class Solution {
   public boolean isPrime(int n) {
