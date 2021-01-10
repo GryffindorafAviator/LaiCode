3655f7744a5670a8e8d8baf37a0d6918c6766b49
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:27:14 -0500

    Create 730. [Debug] areEqual.java

---
+++ 730. [Debug] areEqual.java
@@ -0,0 +1,10 @@
+Debug
+There are bugs in the given code, please fix them.
+
+Check if the given number x and y are equal.
+
+public class Solution {
+  public boolean areEqual(int x, int y) {
+    return x == y;
+  }
+}
