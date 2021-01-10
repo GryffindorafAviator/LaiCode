65acea38d0970901460c15a38b7b0768130b61f0
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:12:18 -0500

    Create 740. [Debug] Hello world.java

---
+++ 740. [Debug] Hello world.java
@@ -0,0 +1,10 @@
+Debug
+There are bugs in the given code, please fix them.
+
+Return String of "Hello world!"
+
+public class Solution {
+  public String helloWorld() {
+    return "Hello world!";
+  }
+}
