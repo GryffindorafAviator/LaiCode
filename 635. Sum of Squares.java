fb05b97c59b60d4f1e5d17b108a6805e86c48cf4
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:08:24 -0500

    Update 635. Sum of Squares.java

--- 635. Sum of Squares.java
+++ 635. Sum of Squares.java
@@ -1,10 +1,10 @@
-Easy
-Problem: Give an array list of integer, calculate the sum of squares of all its elements.
+// Easy
+// Problem: Give an array list of integer, calculate the sum of squares of all its elements.
 
-Note: return 0 if the list is null or empty.
+// Note: return 0 if the list is null or empty.
 
-Example:
-list = {1,2,3} → returns 14 (14=1*1+2*2+3*3)
+// Example:
+// list = {1,2,3} → returns 14 (14=1*1+2*2+3*3)
 
 public class Solution {
   public int sumOfSquare(List<Integer> list) {
