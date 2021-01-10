b0acedd1fbea1283c8dcf3c08b479918392c753a
Ruiyue Wang <phynew@hotmail.com>
2021-01-10 13:35:12 -0500

    Update 732. [Debug] Reverse an array.java

--- 732. [Debug] Reverse an array.java
+++ 732. [Debug] Reverse an array.java
@@ -1,13 +1,13 @@
-Debug
-There are bugs in the given code, please fix them.
+// Debug
+// There are bugs in the given code, please fix them.
 
-Given an array, reverse all its elements.
+// Given an array, reverse all its elements.
 
-Example:
-array = [2, 3, 5, 7, 11]
-result: [11, 7, 5, 3, 2]
+// Example:
+// array = [2, 3, 5, 7, 11]
+// result: [11, 7, 5, 3, 2]
 
-Assumption: The array is not null or empty.
+// Assumption: The array is not null or empty.
 
 public class Solution {
   public void reverse(int[] array) {
