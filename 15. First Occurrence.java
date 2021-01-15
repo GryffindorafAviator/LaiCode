1915d189df8cef38fe12481ca82ce6c446bb4f82
Ruiyue Wang <phynew@hotmail.com>
2021-01-14 23:31:48 -0500

    Update 15. First Occurrence.java

--- 15. First Occurrence.java
+++ 15. First Occurrence.java
@@ -1,16 +1,17 @@
-Medium
-Given a target integer T and an integer array A sorted in ascending order, find the index of the first occurrence of T in A or return -1 if there is no such index.
+// Medium
+// Given a target integer T and an integer array A sorted in ascending order, 
+// find the index of the first occurrence of T in A or return -1 if there is no such index.
 
-Assumptions
-There can be duplicate elements in the array.
+// Assumptions
+// There can be duplicate elements in the array.
 
-Examples
-A = {1, 2, 3}, T = 2, return 1
-A = {1, 2, 3}, T = 4, return -1
-A = {1, 2, 2, 2, 3}, T = 2, return 1
+// Examples
+// A = {1, 2, 3}, T = 2, return 1
+// A = {1, 2, 3}, T = 4, return -1
+// A = {1, 2, 2, 2, 3}, T = 2, return 1
 
-Corner Cases
-What if A is null or A of zero length? We should return -1 in this case.
+// Corner Cases
+// What if A is null or A of zero length? We should return -1 in this case.
 
 public class Solution {
   public int firstOccur(int[] array, int target) {
