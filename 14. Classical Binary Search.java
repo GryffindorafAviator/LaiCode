8b90dd23dece6fcf7eec8c1f02a11b266ebfa5b3
Ruiyue Wang <phynew@hotmail.com>
2021-01-14 21:34:18 -0500

    Update 14. Classical Binary Search.java

--- 14. Classical Binary Search.java
+++ 14. Classical Binary Search.java
@@ -1,17 +1,17 @@
-Easy
-Given a target integer T and an integer array A sorted in ascending order, find the index i such that A[i] == T or return -1 if there is no such index.
+// Easy
+// Given a target integer T and an integer array A sorted in ascending order, find the index i such that A[i] == T or return -1 if there is no such index.
 
-Assumptions
+// Assumptions
 
-There can be duplicate elements in the array, and you can return any of the indices i such that A[i] == T.
-Examples
-
-A = {1, 2, 3, 4, 5}, T = 3, return 2
-A = {1, 2, 3, 4, 5}, T = 6, return -1
-A = {1, 2, 2, 2, 3, 4}, T = 2, return 1 or 2 or 3
-Corner Cases
-
-What if A is null or A is of zero length? We should return -1 in this case.
+// There can be duplicate elements in the array, and you can return any of the indices i such that A[i] == T.
+  
+// Examples
+// A = {1, 2, 3, 4, 5}, T = 3, return 2
+// A = {1, 2, 3, 4, 5}, T = 6, return -1
+// A = {1, 2, 2, 2, 3, 4}, T = 2, return 1 or 2 or 3
+  
+// Corner Cases
+// What if A is null or A is of zero length? We should return -1 in this case.
 
 public class Solution {
   public int binarySearch(int[] array, int target) {
