8ed22f1ffc1876985dbe8029c05096240a2ca6e7
Ruiyue Wang <phynew@hotmail.com>
2021-01-19 11:46:39 -0500

    Update 17. Closest In Sorted Array.java

--- 17. Closest In Sorted Array.java
+++ 17. Closest In Sorted Array.java
@@ -56,3 +56,42 @@ public class Solution {
 }
 
 // Solution
+public class Solution {
+  public int closest(int[] array, int target) {
+    if (array == null || array.length == 0) {
+      return -1;
+    }
+
+    int lt = 0;
+    int rt = array.length - 1;
+
+    while (lt < rt - 1) {
+      int mid = lt + (rt - lt) / 2;
+
+      if (array[mid] == target) {
+        return mid;
+      }
+      else if (array[mid] > target) {
+        rt = mid;
+      }
+      else {
+        lt = mid;
+      }
+    }
+
+    if (target < array[lt]) {
+      return lt;
+    }
+    else if (array[rt] < target) {
+      return rt;
+    }
+    else {
+      if ((target - array[lt]) < (array[rt] - target)) {
+        return lt;
+      }
+      else {
+        return rt;
+      }
+    }
+  }
+}
