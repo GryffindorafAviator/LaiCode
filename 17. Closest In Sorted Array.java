d2aa3cb29873a32dd9e73bee99c2c4bbc26239ab
Ruiyue Wang <phynew@hotmail.com>
2021-01-18 19:45:18 -0500

    Update 17. Closest In Sorted Array.java

--- 17. Closest In Sorted Array.java
+++ 17. Closest In Sorted Array.java
@@ -1,17 +1,17 @@
-Medium
-Given a target integer T and an integer array A sorted in ascending order, find the index i in A such that A[i] is closest to T.
+// Medium
+// Given a target integer T and an integer array A sorted in ascending order, find the index i in A such that A[i] is closest to T.
 
-Assumptions
-There can be duplicate elements in the array, and we can return any of the indices with same value.
+// Assumptions
+// There can be duplicate elements in the array, and we can return any of the indices with same value.
 
-Examples
-A = {1, 2, 3}, T = 2, return 1
-A = {1, 4, 6}, T = 3, return 1
-A = {1, 4, 6}, T = 5, return 1 or 2
-A = {1, 3, 3, 4}, T = 2, return 0 or 1 or 2
+// Examples
+// A = {1, 2, 3}, T = 2, return 1
+// A = {1, 4, 6}, T = 3, return 1
+// A = {1, 4, 6}, T = 5, return 1 or 2
+// A = {1, 3, 3, 4}, T = 2, return 0 or 1 or 2
 
-Corner Cases
-What if A is null or A is of zero length? We should return -1 in this case.
+// Corner Cases
+// What if A is null or A is of zero length? We should return -1 in this case.
 
 public class Solution {
   public int closest(int[] array, int target) {
@@ -55,4 +55,4 @@ public class Solution {
   }
 }
 
-Solution
+// Solution
