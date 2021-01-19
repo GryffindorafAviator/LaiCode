733d89245f6051231a88c5f5119d07ed312a29ba
Ruiyue Wang <phynew@hotmail.com>
2021-01-19 14:05:01 -0500

    Create 561. Find the Kth Element in The Matrix.java

---
+++ 561. Find the Kth Element in The Matrix.java
@@ -0,0 +1,28 @@
+Easy
+Given a matrix, find the Kth index element.
+
+Example: 
+
+matrix:
+1 3 4
+5 6 7
+8 9 10
+
+k = 4 → return: 6
+
+public class Solution {
+  public int findElement(int[][] matrix, int k) {
+    if (matrix == null) {
+      return -1;
+    }
+
+    int row_n = matrix.length;
+    int col_n = matrix[0].length;
+
+    if (k > row_n * col_n) {
+      return -1;
+    }
+
+    return matrix[(k) / col_n][(k) % col_n];
+  }
+}
