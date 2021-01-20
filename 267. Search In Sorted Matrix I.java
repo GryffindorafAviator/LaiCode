cbee4e38bd24b7a4cebe77d47be5712c2455439c
Ruiyue Wang <phynew@hotmail.com>
2021-01-19 20:59:04 -0500

    Update 267. Search In Sorted Matrix I.java

--- 267. Search In Sorted Matrix I.java
+++ 267. Search In Sorted Matrix I.java
@@ -1,17 +1,17 @@
-Medium
-Given a 2D matrix that contains integers only, which each row is sorted in an ascending order. 
-The first element of next row is larger than (or equal to) the last element of previous row.
+// Medium
+// Given a 2D matrix that contains integers only, which each row is sorted in an ascending order. 
+// The first element of next row is larger than (or equal to) the last element of previous row.
 
-Given a target number, returning the position that the target locates within the matrix. 
-If the target number does not exist in the matrix, return {-1, -1}.
+// Given a target number, returning the position that the target locates within the matrix. 
+// If the target number does not exist in the matrix, return {-1, -1}.
 
-Assumptions:
-The given matrix is not null, and has size of N * M, where N >= 0 and M >= 0.
+// Assumptions:
+// The given matrix is not null, and has size of N * M, where N >= 0 and M >= 0.
 
-Examples:
-matrix = { {1, 2, 3}, {4, 5, 7}, {8, 9, 10} }
-target = 7, return {1, 2}
-target = 6, return {-1, -1} to represent the target number does not exist in the matrix.
+// Examples:
+// matrix = { {1, 2, 3}, {4, 5, 7}, {8, 9, 10} }
+// target = 7, return {1, 2}
+// target = 6, return {-1, -1} to represent the target number does not exist in the matrix.
 
 public class Solution {
   public int[] search(int[][] matrix, int target) {
