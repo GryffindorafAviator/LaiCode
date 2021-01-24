0ee0fa836ead8471ec131232dd0b5cc6969d71ad
Ruiyue Wang <phynew@hotmail.com>
2021-01-23 20:23:49 -0500

    Update 550. Calculate Factorial of n.java

--- 550. Calculate Factorial of n.java
+++ 550. Calculate Factorial of n.java
@@ -24,3 +24,14 @@ public class Solution {
     return ans;
   }
 }
+
+// Solution
+public class Solution {
+  public long factorial(int n) {
+    if (n == 0 || n == 1) {
+      return 1;
+    }
+    
+    return n * factorial(n - 1);
+  }
+}
