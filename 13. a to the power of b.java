2efbb3dbd4a36519b5a85252fe34cf44cb523283
Ruiyue Wang <phynew@hotmail.com>
2021-01-23 20:57:45 -0500

    Update 13. a to the power of b.java

--- 13. a to the power of b.java
+++ 13. a to the power of b.java
@@ -39,3 +39,16 @@ public class Solution {
     return a * power(a, b - 1);
   }
 }
+
+// Solution
+public class Solution {
+  public long power(int a, int b) {
+    if(a == 1 || b == 0) {
+      return 1;
+    }
+
+    long half = power(a, b / 2);
+
+    return b % 2 == 0 ? half * half : half * half * a;
+  }
+}
