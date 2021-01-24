f7306cb7103ea3d022791c89a85b31fd36d96bdc
Ruiyue Wang <phynew@hotmail.com>
2021-01-23 20:20:47 -0500

    Create 550. Calculate Factorial of n.java

---
+++ 550. Calculate Factorial of n.java
@@ -0,0 +1,26 @@
+Easy
+Calculate the factorial of n using recursion.
+
+Assumption: n >= 1.
+
+Example:
+n = 1 ⇒ return 1
+
+n = 3 ⇒ return 6
+
+n = 5 ⇒ return 120
+
+public class Solution {
+  public long factorial(int n) {
+    long ans = 1;
+
+    if (n == 0 || n == 1) {
+      return ans;
+    }
+    else {
+      ans = (long)n * factorial(n - 1);
+    }
+
+    return ans;
+  }
+}
