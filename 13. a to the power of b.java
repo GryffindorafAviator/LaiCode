dc13941bb19194924e0af45d49b11feb09d540a0
Ruiyue Wang <phynew@hotmail.com>
2021-01-23 20:30:43 -0500

    Update 13. a to the power of b.java

--- 13. a to the power of b.java
+++ 13. a to the power of b.java
@@ -1,16 +1,16 @@
-Medium
-Evaluate a to the power of b, assuming both a and b are integers and b is non-negative.
+// Medium
+// Evaluate a to the power of b, assuming both a and b are integers and b is non-negative.
 
-Examples
-power(2, 0) = 1
-power(2, 3) = 8
-power(0, 10) = 0
-power(-2, 5) = -32
-Corner Cases
+// Examples
+// power(2, 0) = 1
+// power(2, 3) = 8
+// power(0, 10) = 0
+// power(-2, 5) = -32
+// Corner Cases
 
-In this question, we assume 0^0 = 1.
-What if the result is overflowed? 
-We can assume the result will not be overflowed when we solve this problem on this online judge.
+// In this question, we assume 0^0 = 1.
+// What if the result is overflowed? 
+// We can assume the result will not be overflowed when we solve this problem on this online judge.
 
 public class Solution {
   public long power(int a, int b) {
