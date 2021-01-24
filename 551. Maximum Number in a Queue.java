4b043b26c60b65df5c22ece2357956305f095dd8
Ruiyue Wang <phynew@hotmail.com>
2021-01-24 12:26:03 -0500

    Update 551. Maximum Number in a Queue.java

--- 551. Maximum Number in a Queue.java
+++ 551. Maximum Number in a Queue.java
@@ -1,11 +1,11 @@
-Easy
-Given a queue of integers, find the maximum number in it.
+// Easy
+// Given a queue of integers, find the maximum number in it.
 
-Assumption: the queue is not null or empty.
+// Assumption: the queue is not null or empty.
 
-Example:
-Queue contains 5,8,3,2,7
-Answer: 8.
+// Example:
+// Queue contains 5,8,3,2,7
+// Answer: 8.
 
 class Solution {
 	public int maxInQueue(Queue<Integer> queue) {
