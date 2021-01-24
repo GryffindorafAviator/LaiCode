bfd30f77249a928984d3ab60424fcf9136fbb7d9
Ruiyue Wang <phynew@hotmail.com>
2021-01-24 12:48:40 -0500

    Update 546. Find Index of Minimum Value.java

--- 546. Find Index of Minimum Value.java
+++ 546. Find Index of Minimum Value.java
@@ -1,13 +1,13 @@
-Easy
-Given an array, and a starting index “i”. 
-Find the index of the smallest value from index i (inclusive) to the end of the array.
+// Easy
+// Given an array, and a starting index “i”. 
+// Find the index of the smallest value from index i (inclusive) to the end of the array.
 
-Example:
-array = {1, 9, 6, 8, 7}, i = 1 ⇒ return 2
-array = {0, 3, 9, 8, 5, 4}, i = 2 ⇒ return 5
+// Example:
+// array = {1, 9, 6, 8, 7}, i = 1 ⇒ return 2
+// array = {0, 3, 9, 8, 5, 4}, i = 2 ⇒ return 5
 
-Assumption:
-The array is not null or empty. i is within the boundary of the array.
+// Assumption:
+// The array is not null or empty. i is within the boundary of the array.
 
 public class Solution {
   public int minIndex(int[] array, int i) {
