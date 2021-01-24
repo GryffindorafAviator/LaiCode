534e7017d76a09ae65dba2d0e6c8d7e4baa984c1
Ruiyue Wang <phynew@hotmail.com>
2021-01-24 13:06:36 -0500

    Update 4. Selection Sort.java

--- 4. Selection Sort.java
+++ 4. Selection Sort.java
@@ -1,16 +1,16 @@
-Easy
-Given an array of integers, sort the elements in the array in ascending order. 
-The selection sort algorithm should be used to solve this problem.
+// Easy
+// Given an array of integers, sort the elements in the array in ascending order. 
+// The selection sort algorithm should be used to solve this problem.
 
-Examples
-{1} is sorted to {1}
-{1, 2, 3} is sorted to {1, 2, 3}
-{3, 2, 1} is sorted to {1, 2, 3}
-{4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+// Examples
+// {1} is sorted to {1}
+// {1, 2, 3} is sorted to {1, 2, 3}
+// {3, 2, 1} is sorted to {1, 2, 3}
+// {4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
 
-Corner Cases
-What if the given array is null? In this case, we do not need to do anything.
-What if the given array is of length zero? In this case, we do not need to do anything.
+// Corner Cases
+// What if the given array is null? In this case, we do not need to do anything.
+// What if the given array is of length zero? In this case, we do not need to do anything.
 
 public class Solution {
   public int[] solve(int[] array) {
