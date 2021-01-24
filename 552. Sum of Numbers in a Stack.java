ae444051856db4f99ce02aa92fcb2c69c03ba154
Ruiyue Wang <phynew@hotmail.com>
2021-01-24 12:32:46 -0500

    Create 552. Sum of Numbers in a Stack.java

---
+++ 552. Sum of Numbers in a Stack.java
@@ -0,0 +1,20 @@
+Easy
+Calculate the sum of all numbers in a Stack.
+
+Assumption: The Stack is not null or empty.
+
+Example: 
+Stack contains 7,5,3,0
+Answer: 15
+
+class Solution {
+	public int sumOfStack(Deque<Integer> stack) {
+    int sum = 0;
+
+    while (!stack.isEmpty()) {
+      sum += stack.pollFirst();
+    }
+
+    return sum;
+	}
+}
