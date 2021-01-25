728dea7484b2e74cb36c86dc39b9475b2f0859f9
Ruiyue Wang <phynew@hotmail.com>
2021-01-24 21:37:24 -0500

    Update 624. Fibonacci Number Lite.java

--- 624. Fibonacci Number Lite.java
+++ 624. Fibonacci Number Lite.java
@@ -1,20 +1,20 @@
-Easy
-Get the Kth number in the Fibonacci Sequence. (K is 0-indexed, 
-the 0th Fibonacci number is 0 and the 1st Fibonacci number is 1).
+// Easy
+// Get the Kth number in the Fibonacci Sequence. (K is 0-indexed, 
+// the 0th Fibonacci number is 0 and the 1st Fibonacci number is 1).
 
-Examples
-0th fibonacci number is 0
-1st fibonacci number is 1
-2nd fibonacci number is 1
-3rd fibonacci number is 2
-6th fibonacci number is 8
+// Examples
+// 0th fibonacci number is 0
+// 1st fibonacci number is 1
+// 2nd fibonacci number is 1
+// 3rd fibonacci number is 2
+// 6th fibonacci number is 8
 
-Corner Cases
-What if K < 0? in this case, we should always return 0.
-Is it possible the result fibonacci number is overflowed? 
-We can assume it will not be overflowed when we solve this problem on this online judge, 
-but we should also know that it is possible to get an overflowed number, 
-and sometimes we will need to use something like BigInteger.
+// Corner Cases
+// What if K < 0? in this case, we should always return 0.
+// Is it possible the result fibonacci number is overflowed? 
+// We can assume it will not be overflowed when we solve this problem on this online judge, 
+// but we should also know that it is possible to get an overflowed number, 
+// and sometimes we will need to use something like BigInteger.
 
 public class Solution {
   public int fibonacci(int K) { 
