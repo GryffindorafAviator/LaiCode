9f6e085f294f4adff64c8cf8b3156d1a5fe9352c
Ruiyue Wang <phynew@hotmail.com>
2021-01-26 17:55:02 -0500

    Update 31. Queue By Two Stacks.java

--- 31. Queue By Two Stacks.java
+++ 31. Queue By Two Stacks.java
@@ -1,18 +1,18 @@
-Medium
-Java: Implement a queue by using two stacks. 
-The queue should provide size(), isEmpty(), 
-offer(), poll() and peek() operations. 
-When the queue is empty, poll() and peek() should return null.
+// Medium
+// Java: Implement a queue by using two stacks. 
+// The queue should provide size(), isEmpty(), 
+// offer(), poll() and peek() operations. 
+// When the queue is empty, poll() and peek() should return null.
 
-C++: Implement a queue by using two stacks. 
-The queue should provide size(), isEmpty(), 
-push(), front() and pop() operations. When the queue is empty, front() should return -1.
+// C++: Implement a queue by using two stacks. 
+// The queue should provide size(), isEmpty(), 
+// push(), front() and pop() operations. When the queue is empty, front() should return -1.
 
-Assumptions
-The elements in the queue are all Integers.
-size() should return the number of elements buffered in the queue.
-isEmpty() should return true if there is no element buffered in the queue, 
-false otherwise.
+// Assumptions
+// The elements in the queue are all Integers.
+// size() should return the number of elements buffered in the queue.
+// isEmpty() should return true if there is no element buffered in the queue, 
+// false otherwise.
 
 public class Solution {
   private LinkedList<Integer> in;
