a47524093663bfc58733e2da6137396466c6aae0
Ruiyue Wang <phynew@hotmail.com>
2021-01-25 21:09:57 -0500

    Create 553. Shuffle Stacks.java

---
+++ 553. Shuffle Stacks.java
@@ -0,0 +1,18 @@
+Easy
+Given a stack of numbers, 
+shuffle all numbers into another Stack so that the order is reversed.
+
+Assumption: the stack is not null or empty.
+
+Example:
+stack1 = [8,4,6,1]  stack2 = []
+After shuffle:
+stack1 = [], stack2 = [1, 6, 4, 8]
+
+class Solution {
+	public void shuffle(Deque<Integer> stack1, Deque<Integer> stack2) {
+    while (!stack1.isEmpty()) {
+      stack2.offerFirst(stack1.pollFirst());
+    }
+	}
+}
