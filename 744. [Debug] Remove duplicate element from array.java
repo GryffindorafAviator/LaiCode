d443e596520fe4180c476fe51caf7afb669a13b5
Ruiyue Wang <phynew@hotmail.com>
2021-01-25 20:31:47 -0500

    Update 744. [Debug] Remove duplicate element from array.java

--- 744. [Debug] Remove duplicate element from array.java
+++ 744. [Debug] Remove duplicate element from array.java
@@ -1,15 +1,15 @@
-Debug
-There are bugs in the given code, please fix them.
+// Debug
+// There are bugs in the given code, please fix them.
 
-Given a 1D array and a known duplicate element (which appears more than once), 
-remove the duplicate element and return the result array.
+// Given a 1D array and a known duplicate element (which appears more than once), 
+// remove the duplicate element and return the result array.
 
-Input: array = [2, 3, 3, 4, 5], duplicate = 3
-Output: [2, 4, 5]
+// Input: array = [2, 3, 3, 4, 5], duplicate = 3
+// Output: [2, 4, 5]
 
-Assume
-(1) the input array is not null or empty 
-(2) the duplicate element always exists in the input array and there is only one duplicate element.
+// Assume
+// (1) the input array is not null or empty 
+// (2) the duplicate element always exists in the input array and there is only one duplicate element.
 
 public class Solution {
  public int[] removeDuplicate(int[] array, int duplicate) {
