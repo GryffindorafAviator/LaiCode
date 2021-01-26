72bc4cd5da276c50e14eeaf9ac55477e6a2b07b2
Ruiyue Wang <phynew@hotmail.com>
2021-01-25 20:27:44 -0500

    Update 743. [Debug] a to the power of b.java

--- 743. [Debug] a to the power of b.java
+++ 743. [Debug] a to the power of b.java
@@ -1,18 +1,18 @@
-Debug
-There are bugs in the given code, please fix them.
+// Debug
+// There are bugs in the given code, please fix them.
 
-Evaluate a to the power of b, assuming both a and b are integers and b is non-negative.
+// Evaluate a to the power of b, assuming both a and b are integers and b is non-negative.
 
-Examples
-power(2, 0) = 1
-power(2, 3) = 8
-power(0, 10) = 0
-power(-2, 5) = -32
+// Examples
+// power(2, 0) = 1
+// power(2, 3) = 8
+// power(0, 10) = 0
+// power(-2, 5) = -32
 
-Corner Cases
-In this question, we assume 0^0 = 1.
-What if the result is overflowed? 
-We can assume the result will not be overflowed when we solve this problem on this online judge.
+// Corner Cases
+// In this question, we assume 0^0 = 1.
+// What if the result is overflowed? 
+// We can assume the result will not be overflowed when we solve this problem on this online judge.
 
 public class Solution {
  public long power(int a, int b) {
