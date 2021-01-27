e6fe00ee790e11b2678fc5a609de1060973ad945
Ruiyue Wang <phynew@hotmail.com>
2021-01-26 20:24:23 -0500

    Create 555. Count Nodes in a LinkedList.java

---
+++ 555. Count Nodes in a LinkedList.java
@@ -0,0 +1,33 @@
+Easy
+Given a linked list, 
+count the number of nodes in it.
+
+Assumption: The linkedlist is not null or empty.
+
+Example:
+Given: 1->7->5->4->2->null
+Answer: 5
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public int count(ListNode head) {
+    int cnt = 0;
+    ListNode cur = head;
+
+    while (cur != null) {
+      ++cnt;
+      cur = cur.next;
+    }
+
+    return cnt;
+  }
+}
