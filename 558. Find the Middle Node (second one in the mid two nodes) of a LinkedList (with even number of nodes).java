bad08f813363ec8da33710c74f7658622335d74b
Ruiyue Wang <phynew@hotmail.com>
2021-01-26 22:59:24 -0500

    Create 558. Find the Middle Node (second one in the mid two nodes) of a LinkedList (with even number of nodes).java

---
+++ 558. Find the Middle Node (second one in the mid two nodes) of a LinkedList (with even number of nodes).java
@@ -0,0 +1,34 @@
+Easy
+Find the middle node (second one in the mid two nodes) of a given linked list with even number length.
+
+Example: 
+L = null, return null
+L = 1 -> 2 -> 3 -> 4 null, return 3
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode findMiddleNode(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while (fast.next.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+
+    return slow.next;
+  }
+}
