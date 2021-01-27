9dd9fac572913ebfde26d6f78e3cb0befcbc9bee
Ruiyue Wang <phynew@hotmail.com>
2021-01-27 14:26:25 -0500

    Update 39. Insert In Sorted Linked List.java

--- 39. Insert In Sorted Linked List.java
+++ 39. Insert In Sorted Linked List.java
@@ -1,11 +1,11 @@
-Easy
-Insert a value in a sorted linked list.
+// Easy
+// Insert a value in a sorted linked list.
 
-Examples
-L = null, insert 1, return 1 -> null
-L = 1 -> 3 -> 5 -> null, insert 2, return 1 -> 2 -> 3 -> 5 -> null
-L = 1 -> 3 -> 5 -> null, insert 3, return 1 -> 3 -> 3 -> 5 -> null
-L = 2 -> 3 -> null, insert 1, return 1 -> 2 -> 3 -> null
+// Examples
+// L = null, insert 1, return 1 -> null
+// L = 1 -> 3 -> 5 -> null, insert 2, return 1 -> 2 -> 3 -> 5 -> null
+// L = 1 -> 3 -> 5 -> null, insert 3, return 1 -> 3 -> 3 -> 5 -> null
+// L = 2 -> 3 -> null, insert 1, return 1 -> 2 -> 3 -> null
 
 /**
  * class ListNode {
