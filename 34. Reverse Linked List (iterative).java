4d124bf276dce853ebc1be9aa11ecacc792e7ce0
Ruiyue Wang <phynew@hotmail.com>
2021-01-26 20:35:00 -0500

    Create 34. Reverse Linked List (iterative).java

---
+++ 34. Reverse Linked List (iterative).java
@@ -0,0 +1,40 @@
+Easy
+Reverse a singly-linked list iteratively.
+
+Examples
+L = null, return null
+L = 1 -> null, return 1 -> null
+L = 1 -> 2 -> 3 -> null, return 3 -> 2 -> 1 -> null
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode reverse(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode pre = null;
+    ListNode cur = head;
+    ListNode nxt = null;
+
+    while (cur != null) {
+      nxt = cur.next;
+      cur.next = pre;
+      pre = cur;
+      cur = nxt;
+    }
+
+    head = pre;
+
+    return head;
+  }
+}
