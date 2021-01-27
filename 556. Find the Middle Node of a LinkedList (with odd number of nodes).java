5efa8f7a43164fcf43188eb6d68d0503983f2ec2
Ruiyue Wang <phynew@hotmail.com>
2021-01-26 22:49:04 -0500

    Create 556. Find the Middle Node of a LinkedList (with odd number of nodes).java

---
+++ 556. Find the Middle Node of a LinkedList (with odd number of nodes).java
@@ -0,0 +1,35 @@
+Easy
+Find the middle node of a given linked list with odd number length.
+
+Assumption: the length > 0
+
+Example: 
+L = 1 -> 2 -> 3 -> null, return 2->3
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode findMiddleNode(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while (fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+
+    return slow;
+  }
+}
