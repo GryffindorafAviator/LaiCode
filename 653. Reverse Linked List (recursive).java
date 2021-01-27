2e36fa3b56e8863f946af0c7a83c8b95f45cca6f
Ruiyue Wang <phynew@hotmail.com>
2021-01-26 22:40:06 -0500

    Create 653. Reverse Linked List (recursive).java

---
+++ 653. Reverse Linked List (recursive).java
@@ -0,0 +1,31 @@
+Easy
+Reverse a singly-linked list recursively.
+
+Examples
+L = null, return null
+L = 1 -> null, return 1 -> null
+L = 1 -> 2 -> 3 -> null, return 3 -> 2 -> 1 -> null
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode reverse(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode newHead = reverse(head.next);
+    head.next.next = head;
+    head.next = null;
+
+    return newHead;
+  }
+}
