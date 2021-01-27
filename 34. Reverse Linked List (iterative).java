f157272ee8cbb53d7d918980da01e20ba08f15c8
Ruiyue Wang <phynew@hotmail.com>
2021-01-26 20:39:40 -0500

    Update 34. Reverse Linked List (iterative).java

--- 34. Reverse Linked List (iterative).java
+++ 34. Reverse Linked List (iterative).java
@@ -38,3 +38,29 @@ public class Solution {
     return head;
   }
 }
+
+// Solution
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode reverse(ListNode head) {
+    ListNode pre = null;
+
+    while (head != null) {
+      ListNode nxt = head.next;
+      head.next = pre;
+      pre = head;
+      head = nxt;
+    }
+
+    return pre;
+  }
+}
