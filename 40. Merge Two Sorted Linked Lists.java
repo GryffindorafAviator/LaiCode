09e8f0ab5d28bf506765ebd59101c1e2c7ba70d5
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 11:56:38 -0500

    Update 40. Merge Two Sorted Linked Lists.java

--- 40. Merge Two Sorted Linked Lists.java
+++ 40. Merge Two Sorted Linked Lists.java
@@ -1,11 +1,11 @@
-Easy
-Merge two sorted lists into one large sorted list.
+// Easy
+// Merge two sorted lists into one large sorted list.
 
-Examples
+// Examples
 
-L1 = 1 -> 4 -> 6 -> null, L2 = 2 -> 5 -> null, merge L1 and L2 to 1 -> 2 -> 4 -> 5 -> 6 -> null
-L1 = null, L2 = 1 -> 2 -> null, merge L1 and L2 to 1 -> 2 -> null
-L1 = null, L2 = null, merge L1 and L2 to null
+// L1 = 1 -> 4 -> 6 -> null, L2 = 2 -> 5 -> null, merge L1 and L2 to 1 -> 2 -> 4 -> 5 -> 6 -> null
+// L1 = null, L2 = 1 -> 2 -> null, merge L1 and L2 to 1 -> 2 -> null
+// L1 = null, L2 = null, merge L1 and L2 to null
 
 /**
  * class ListNode {
