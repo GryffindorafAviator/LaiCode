3fb4e82ff1eb8d6e8a327e45f092b3b0cb01e61a
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 16:28:44 -0500

    Create 654. In-order Traversal Of Binary Tree (recursive).java

---
+++ 654. In-order Traversal Of Binary Tree (recursive).java
@@ -0,0 +1,65 @@
+Easy
+Implement a recursive, in-order traversal of a given binary tree, 
+return the list of keys of each node in the tree as it is in-order traversed.
+
+Examples
+
+        5
+
+      /    \
+
+    3        8
+
+  /   \        \
+
+1      4        11
+
+In-order traversal is [1, 3, 4, 5, 8, 11]
+
+Corner Cases
+What if the given binary tree is null? Return an empty list in this case.
+How is the binary tree represented?
+We use the level order traversal sequence with a special symbol "#" denoting the null node.
+
+For Example:
+The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4
+    
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public List<Integer> inOrder(TreeNode root) {
+    List<Integer> ans = new ArrayList<>();
+
+    traversal(ans, root);
+
+    return ans;
+  }
+
+  private void traversal(List<Integer> ans, TreeNode root) {
+    if (root == null) {
+      return;
+    }
+
+    traversal(ans, root.left);
+    ans.add(root.key);   
+    traversal(ans, root.right);
+  }
+}
