1f2875a23649fc37f865f8b741f4d052bb97aeb8
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 16:29:26 -0500

    Update 655. Pre-order Traversal Of Binary Tree (recursive).java

--- 655. Pre-order Traversal Of Binary Tree (recursive).java
+++ 655. Pre-order Traversal Of Binary Tree (recursive).java
@@ -1,39 +1,39 @@
-Easy
-Implement a recursive, pre-order traversal of a given binary tree, 
-return the list of keys of each node in the tree as it is pre-order traversed.
+// Easy
+// Implement a recursive, pre-order traversal of a given binary tree, 
+// return the list of keys of each node in the tree as it is pre-order traversed.
 
-Examples
+// Examples
 
-        5
+//         5
 
-      /    \
+//       /    \
 
-    3        8
+//     3        8
 
-  /   \        \
+//   /   \        \
 
-1      4        11
+// 1      4        11
 
-Pre-order traversal is [5, 3, 1, 4, 8, 11]
+// Pre-order traversal is [5, 3, 1, 4, 8, 11]
 
-Corner Cases
-What if the given binary tree is null? 
-Return an empty list in this case.
-How is the binary tree represented?
-We use the level order traversal sequence with a special symbol "#" denoting the null node.
+// Corner Cases
+// What if the given binary tree is null? 
+// Return an empty list in this case.
+// How is the binary tree represented?
+// We use the level order traversal sequence with a special symbol "#" denoting the null node.
 
-For Example:
-The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+// For Example:
+// The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
 
-    1
+//     1
 
-  /   \
+//   /   \
 
- 2     3
+//  2     3
 
-      /
+//       /
 
-    4
+//     4
     
 /**
  * public class TreeNode {
