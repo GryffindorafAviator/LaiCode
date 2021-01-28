81005f699223f9f880c05e312f1ef66b3e2730ed
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 15:10:08 -0500

    Update 42. Partition Linked List.java

--- 42. Partition Linked List.java
+++ 42. Partition Linked List.java
@@ -1,10 +1,10 @@
-Medium
-Given a linked list and a target value T, 
-partition it such that all nodes less than T are listed before the nodes larger than or equal to target value T. 
-The original relative order of the nodes in each of the two partitions should be preserved.
+// Medium
+// Given a linked list and a target value T, 
+// partition it such that all nodes less than T are listed before the nodes larger than or equal to target value T. 
+// The original relative order of the nodes in each of the two partitions should be preserved.
 
-Examples
-L = 2 -> 4 -> 3 -> 5 -> 1 -> null, T = 3, is partitioned to 2 -> 1 -> 4 -> 3 -> 5 -> null
+// Examples
+// L = 2 -> 4 -> 3 -> 5 -> 1 -> null, T = 3, is partitioned to 2 -> 1 -> 4 -> 3 -> 5 -> null
 
 /**
  * class ListNode {
