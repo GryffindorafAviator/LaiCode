30ecd80b42931469c9d9498fa03e3caa1b66f07a
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 16:37:38 -0500

    Update 656. Post-order Traversal Of Binary Tree (recursive).java

--- 656. Post-order Traversal Of Binary Tree (recursive).java
+++ 656. Post-order Traversal Of Binary Tree (recursive).java
@@ -1,41 +1,41 @@
-Easy
-Implement a recursive, 
-post-order traversal of a given binary tree, 
-return the list of keys of each node in the tree as it is post-order traversed.
+// Easy
+// Implement a recursive, 
+// post-order traversal of a given binary tree, 
+// return the list of keys of each node in the tree as it is post-order traversed.
 
-Examples
+// Examples
 
-        5
+//         5
 
-      /    \
+//       /    \
 
-    3        8
+//     3        8
 
-  /   \        \
+//   /   \        \
 
-1      4        11
+// 1      4        11
 
-Post-order traversal is [1, 4, 3, 11, 8, 5]
+// Post-order traversal is [1, 4, 3, 11, 8, 5]
 
-Corner Cases
-What if the given binary tree is null? 
-Return an empty list in this case.
-How is the binary tree represented?
+// Corner Cases
+// What if the given binary tree is null? 
+// Return an empty list in this case.
+// How is the binary tree represented?
 
-We use the level order traversal sequence with a special symbol "#" denoting the null node.
+// We use the level order traversal sequence with a special symbol "#" denoting the null node.
 
-For Example:
-The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+// For Example:
+// The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
 
-    1
+//     1
 
-  /   \
+//   /   \
 
- 2     3
+//  2     3
 
-      /
+//       /
 
-    4
+//     4
 
 /**
  * public class TreeNode {
