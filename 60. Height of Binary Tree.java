101a6ba1b9ef578f31ea6ec92d73b738712352a0
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 17:24:11 -0500

    Create 60. Height of Binary Tree.java

---
+++ 60. Height of Binary Tree.java
@@ -0,0 +1,57 @@
+Easy
+Find the height of binary tree.
+
+Examples:
+
+        5
+
+      /    \
+
+    3        8
+
+  /   \        \
+
+1      4        11
+
+The height of above binary tree is 3.
+
+How is the binary tree represented?
+
+We use the level order traversal sequence with a special symbol "#" denoting the null node.
+
+For Example:
+The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4  
+    
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public int findHeight(TreeNode root) {
+     if (root == null) {
+      return 0;
+    }
+
+    int leftHeight = findHeight(root.left);
+    int rightHeight = findHeight(root.right);
+    int height = Math.max(leftHeight, rightHeight) + 1;
+
+    return height;
+  }
+}
