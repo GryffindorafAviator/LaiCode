ec7bf1410fbb5f77d3b4d83179e62138d4ee3a30
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 20:07:19 -0500

    Create 548. Bounded Random Number.java

---
+++ 548. Bounded Random Number.java
@@ -0,0 +1,14 @@
+Easy
+Write a function that returns a random number within the range of [a, b] with equal probabilities.
+
+Note: you can use Java’s Random class.
+
+class Solution {
+	public int random(int a, int b) {
+    Random rand = new Random();
+
+    int num = a + rand.nextInt(b - a + 1);
+
+    return num;
+	}
+}
