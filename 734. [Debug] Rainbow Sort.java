29bc3401481fbcb0218456906ad09ef8005b374c
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 22:27:04 -0500

    Update 734. [Debug] Rainbow Sort.java

--- 734. [Debug] Rainbow Sort.java
+++ 734. [Debug] Rainbow Sort.java
@@ -1,22 +1,22 @@
-Debug
-There are bugs in the given code, please fix them.
+// Debug
+// There are bugs in the given code, please fix them.
 
-Given an array of balls, where the color of the balls can only be Red, Green or Blue, 
-sort the balls such that all the Red balls are grouped on the left side, 
-all the Green balls are grouped in the middle and all the Blue balls are grouped on the right side. 
-(Red is denoted by -1, Green is denoted by 0, and Blue is denoted by 1).
+// Given an array of balls, where the color of the balls can only be Red, Green or Blue, 
+// sort the balls such that all the Red balls are grouped on the left side, 
+// all the Green balls are grouped in the middle and all the Blue balls are grouped on the right side. 
+// (Red is denoted by -1, Green is denoted by 0, and Blue is denoted by 1).
 
-Examples
-{0} is sorted to {0}
-{1, 0} is sorted to {0, 1}
-{1, 0, 1, -1, 0} is sorted to {-1, 0, 0, 1, 1}
+// Examples
+// {0} is sorted to {0}
+// {1, 0} is sorted to {0, 1}
+// {1, 0, 1, -1, 0} is sorted to {-1, 0, 0, 1, 1}
 
-Assumptions
-The input array is not null.
+// Assumptions
+// The input array is not null.
 
-Corner Cases
-What if the input array is of length zero? 
-In this case, we should not do anything as well.
+// Corner Cases
+// What if the input array is of length zero? 
+// In this case, we should not do anything as well.
 
 public class Solution {
   public int[] rainbowSort(int[] array) {
