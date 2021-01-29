35554e66e505620d1c9cdd05f9741f86d32b2f9e
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 22:24:11 -0500

    Update 11. Rainbow Sort.java

--- 11. Rainbow Sort.java
+++ 11. Rainbow Sort.java
@@ -1,20 +1,20 @@
-Medium
-Given an array of balls, where the color of the balls can only be Red, 
-Green or Blue, 
-sort the balls such that all the Red balls are grouped on the left side, 
-all the Green balls are grouped in the middle and all the Blue balls are grouped on the right side. 
-(Red is denoted by -1, Green is denoted by 0, and Blue is denoted by 1).
+// Medium
+// Given an array of balls, where the color of the balls can only be Red, 
+// Green or Blue, 
+// sort the balls such that all the Red balls are grouped on the left side, 
+// all the Green balls are grouped in the middle and all the Blue balls are grouped on the right side. 
+// (Red is denoted by -1, Green is denoted by 0, and Blue is denoted by 1).
 
-Examples
-{0} is sorted to {0}
-{1, 0} is sorted to {0, 1}
-{1, 0, 1, -1, 0} is sorted to {-1, 0, 0, 1, 1}
+// Examples
+// {0} is sorted to {0}
+// {1, 0} is sorted to {0, 1}
+// {1, 0, 1, -1, 0} is sorted to {-1, 0, 0, 1, 1}
 
-Assumptions
-The input array is not null.
-Corner Cases
-What if the input array is of length zero? 
-In this case, we should not do anything as well.
+// Assumptions
+// The input array is not null.
+// Corner Cases
+// What if the input array is of length zero? 
+// In this case, we should not do anything as well.
 
 public class Solution {
   public int[] rainbowSort(int[] array) {
