848c8192d4cce22cbc77fdef0a0b4ce371c0793b
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 22:45:22 -0500

    Create 735. [Debug] Reverse Linked List.java

---
+++ 735. [Debug] Reverse Linked List.java
@@ -0,0 +1,22 @@
+Debug
+There are bugs in the given code, please fix them.
+
+Reverse a singly-linked list.
+
+Examples
+
+L = null, return null
+L = 1 -> null, return 1 -> null
+L = 1 -> 2 -> 3 -> null, return 3 -> 2 -> 1 -> null
+
+public class Solution {
+  public ListNode reverse(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode newHead = reverse(head.next);
+    head.next.next = head;
+    head.next = null;
+    return newHead;
+  }
+}
