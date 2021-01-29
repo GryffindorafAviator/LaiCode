3a08aee4658bfb4da4c4768c862cf7aef3f477a7
Ruiyue Wang <phynew@hotmail.com>
2021-01-28 21:28:48 -0500

    Update 10. Quick Sort.java

--- 10. Quick Sort.java
+++ 10. Quick Sort.java
@@ -1,16 +1,16 @@
-Medium
-Given an array of integers, sort the elements in the array in ascending order. 
-The quick sort algorithm should be used to solve this problem.
+// Medium
+// Given an array of integers, sort the elements in the array in ascending order. 
+// The quick sort algorithm should be used to solve this problem.
 
-Examples
-{1} is sorted to {1}
-{1, 2, 3} is sorted to {1, 2, 3}
-{3, 2, 1} is sorted to {1, 2, 3}
-{4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+// Examples
+// {1} is sorted to {1}
+// {1, 2, 3} is sorted to {1, 2, 3}
+// {3, 2, 1} is sorted to {1, 2, 3}
+// {4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
 
-Corner Cases
-What if the given array is null? In this case, we do not need to do anything.
-What if the given array is of length zero? In this case, we do not need to do anything.
+// Corner Cases
+// What if the given array is null? In this case, we do not need to do anything.
+// What if the given array is of length zero? In this case, we do not need to do anything.
 
 public class Solution {
   public int[] quickSort(int[] array) {
