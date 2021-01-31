c4344b48c0b6a88eb92591b181346d91c8e80b6e
Ruiyue Wang <phynew@hotmail.com>
2021-01-31 14:35:46 -0500

    Update 46. Check If Binary Tree Is Balanced.java

--- 46. Check If Binary Tree Is Balanced.java
+++ 46. Check If Binary Tree Is Balanced.java
@@ -84,3 +84,48 @@ public class Solution {
     return height;
   }
 }
+
+// Solution
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean isBalanced(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+
+    return height(root) != -1;
+  }
+
+  private int height(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftHeight = height(root.left);
+ 
+    if (leftHeight == -1) {
+      return -1;
+    }
+
+    int rightHeight = height(root.right);
+
+    if (rightHeight == -1) {
+      return -1;
+    }
+
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+}
