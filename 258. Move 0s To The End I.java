556520be59f860cf57aabcaa8b4b9041a9e21828
Ruiyue Wang <phynew@hotmail.com>
2021-02-23 20:42:19 -0500

    Create 258. Move 0s To The End I.java

---
+++ 258. Move 0s To The End I.java
@@ -0,0 +1,39 @@
+Easy
+Given an array of integers, move all the 0s to the right end of the array.
+The relative order of the elements in the original array does not need to be maintained.
+
+Assumptions:
+The given array is not null.
+  
+Examples:
+{1} --> {1}
+{1, 0, 3, 0, 1} --> {1, 3, 1, 0, 0} or {1, 1, 3, 0, 0} or {3, 1, 1, 0, 0}
+
+public class Solution {
+  public int[] moveZero(int[] array) {
+    if (array.length == 0) {
+      return array;
+    }
+
+    int i = 0;
+    int j = array.length - 1;
+
+    while (i <= j) {
+      if (array[i] == 0) {
+        swap(array, i, j);
+        --j;
+      }
+      else {
+        ++i;
+      }
+    }
+
+    return array;
+  }
+
+  private void swap(int[] array, int i, int j) {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
