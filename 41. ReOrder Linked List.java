a69bd8542168efaca003269f004a9251bf539471
Ruiyue Wang <phynew@hotmail.com>
2021-02-27 13:07:06 -0500

    Update 41. ReOrder Linked List.java

--- 41. ReOrder Linked List.java
+++ 41. ReOrder Linked List.java
@@ -1,11 +1,11 @@
-Hard
-Reorder the given singly-linked list N1 -> N2 -> N3 -> N4 -> … -> Nn -> null to be N1- > Nn -> N2 -> Nn-1 -> N3 -> Nn-2 -> … -> null
+// Hard
+// Reorder the given singly-linked list N1 -> N2 -> N3 -> N4 -> … -> Nn -> null to be N1- > Nn -> N2 -> Nn-1 -> N3 -> Nn-2 -> … -> null
 
-Examples
-L = null, is reordered to null
-L = 1 -> null, is reordered to 1 -> null
-L = 1 -> 2 -> 3 -> 4 -> null, is reordered to 1 -> 4 -> 2 -> 3 -> null
-L = 1 -> 2 -> 3 -> null, is reordred to 1 -> 3 -> 2 -> null
+// Examples
+// L = null, is reordered to null
+// L = 1 -> null, is reordered to 1 -> null
+// L = 1 -> 2 -> 3 -> 4 -> null, is reordered to 1 -> 4 -> 2 -> 3 -> null
+// L = 1 -> 2 -> 3 -> null, is reordred to 1 -> 3 -> 2 -> null
   
   /**
  * class ListNode {
