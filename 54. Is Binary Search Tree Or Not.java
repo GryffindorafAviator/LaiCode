084c079e1482587e44c4e15f53743bb0d016f707
Ruiyue Wang <phynew@hotmail.com>
2021-04-30 19:02:08 -0400

    Update 54. Is Binary Search Tree Or Not.java

--- 54. Is Binary Search Tree Or Not.java
+++ 54. Is Binary Search Tree Or Not.java
@@ -1,12 +1,12 @@
-Medium
-Determine if a given binary tree is binary search tree.There should no be duplicate keys in binary search tree.
+// Medium
+// Determine if a given binary tree is binary search tree.There should no be duplicate keys in binary search tree.
 
-Assumptions
+// Assumptions
 
-You can assume the keys stored in the binary search tree can not be Integer.MIN_VALUE or Integer.MAX_VALUE.
-Corner Cases
+// You can assume the keys stored in the binary search tree can not be Integer.MIN_VALUE or Integer.MAX_VALUE.
+// Corner Cases
 
-What if the binary tree is null? Return true in this case.
+// What if the binary tree is null? Return true in this case.
   
 /**
  * public class TreeNode {
