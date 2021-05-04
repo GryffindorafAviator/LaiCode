a2098abf68f45b84ecd8484d363ef730ed290e37
Ruiyue Wang <phynew@hotmail.com>
2021-05-03 22:41:42 -0400

    Update 55. Get Keys In Binary Search Tree In Given Range.java

--- 55. Get Keys In Binary Search Tree In Given Range.java
+++ 55. Get Keys In Binary Search Tree In Given Range.java
@@ -1,40 +1,40 @@
-Easy
-Get the list of keys in a given binary search tree in a given range[min, max] in ascending order, both min and max are inclusive.
+// Easy
+// Get the list of keys in a given binary search tree in a given range[min, max] in ascending order, both min and max are inclusive.
 
-Examples
+// Examples
 
-        5
+//         5
 
-      /    \
+//       /    \
 
-    3        8
+//     3        8
 
-  /   \        \
+//   /   \        \
 
- 1     4        11
+//  1     4        11
 
-get the keys in [2, 5] in ascending order, result is  [3, 4, 5]
+// get the keys in [2, 5] in ascending order, result is  [3, 4, 5]
 
-Corner Cases
+// Corner Cases
 
-What if there are no keys in the given range? Return an empty list in this case.
-How is the binary tree represented?
+// What if there are no keys in the given range? Return an empty list in this case.
+// How is the binary tree represented?
 
-We use the level order traversal sequence with a special symbol "#" denoting the null node.
+// We use the level order traversal sequence with a special symbol "#" denoting the null node.
 
-For Example:
+// For Example:
 
-The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+// The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
 
-    1
+//     1
 
-  /   \
+//   /   \
 
- 2     3
+//  2     3
 
-      /
+//       /
 
-    4
+//     4
       
 /**
  * public class TreeNode {
