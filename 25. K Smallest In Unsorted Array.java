cdc086a6dc7505926ccd976b2bf161dc77472edc
Ruiyue Wang <phynew@hotmail.com>
2021-05-27 20:29:22 -0400

    Update 25. K Smallest In Unsorted Array.java

--- 25. K Smallest In Unsorted Array.java
+++ 25. K Smallest In Unsorted Array.java
@@ -1,16 +1,16 @@
-Medium
-Find the K smallest numbers in an unsorted integer array A. The returned numbers should be in ascending order.
+// Medium
+// Find the K smallest numbers in an unsorted integer array A. The returned numbers should be in ascending order.
 
-Assumptions
+// Assumptions
 
-A is not null
-K is >= 0 and smaller than or equal to size of A
-Return
+// A is not null
+// K is >= 0 and smaller than or equal to size of A
+// Return
 
-an array with size K containing the K smallest numbers in ascending order
-Examples
+// an array with size K containing the K smallest numbers in ascending order
+// Examples
 
-A = {3, 4, 1, 2, 5}, K = 3, the 3 smallest numbers are {1, 2, 3}
+// A = {3, 4, 1, 2, 5}, K = 3, the 3 smallest numbers are {1, 2, 3}
 
 public class Solution {
   public int[] kSmallest(int[] array, int k) {
