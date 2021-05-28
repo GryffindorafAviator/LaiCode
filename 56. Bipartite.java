bca01f0603ac9c7a4bef265e7c0521971cd6c954
Ruiyue Wang <phynew@hotmail.com>
2021-05-28 00:19:59 -0400

    Update 56. Bipartite.java

--- 56. Bipartite.java
+++ 56. Bipartite.java
@@ -1,27 +1,27 @@
-Hard
-Determine if an undirected graph is bipartite. A bipartite graph is one in which the nodes can be divided into two groups such that no nodes have direct edges to other nodes in the same group.
+// Hard
+// Determine if an undirected graph is bipartite. A bipartite graph is one in which the nodes can be divided into two groups such that no nodes have direct edges to other nodes in the same group.
 
-Examples
+// Examples
 
-1  --   2
+// 1  --   2
 
-    /   
+//     /   
 
-3  --   4
+// 3  --   4
 
-is bipartite (1, 3 in group 1 and 2, 4 in group 2).
+// is bipartite (1, 3 in group 1 and 2, 4 in group 2).
 
-1  --   2
+// 1  --   2
 
-    /   |
+//     /   |
 
-3  --   4
+// 3  --   4
 
-is not bipartite.
+// is not bipartite.
 
-Assumptions
+// Assumptions
 
-The graph is represented by a list of nodes, and the list of nodes is not null.
+// The graph is represented by a list of nodes, and the list of nodes is not null.
 
 /**
  * public class GraphNode {
