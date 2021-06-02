5e5cad09b1f7b53254f7ca0f8baae6e6d6ccca70
Ruiyue Wang <phynew@hotmail.com>
2021-06-02 19:40:18 -0400

    Update 395. Remove Certain Characters.java

--- 395. Remove Certain Characters.java
+++ 395. Remove Certain Characters.java
@@ -1,13 +1,13 @@
-Easy
-Remove given characters in input string, the relative order of other characters should be remained. Return the new string after deletion.
+// Easy
+// Remove given characters in input string, the relative order of other characters should be remained. Return the new string after deletion.
 
-Assumptions
+// Assumptions
 
-The given input string is not null.
-The characters to be removed is given by another string, it is guaranteed to be not null.
-Examples
+// The given input string is not null.
+// The characters to be removed is given by another string, it is guaranteed to be not null.
+// Examples
 
-input = "abcd", t = "ab", delete all instances of 'a' and 'b', the result is "cd".
+// input = "abcd", t = "ab", delete all instances of 'a' and 'b', the result is "cd".
   
 public class Solution {
   public String remove(String input, String t) {
