c4d843a7d28ac715362be93103d652a4211fb53f
Ruiyue Wang <phynew@hotmail.com>
2021-06-02 16:12:09 -0400

    Update 68. Missing Number I.java

--- 68. Missing Number I.java
+++ 68. Missing Number I.java
@@ -48,3 +48,24 @@ public class Solution {
     return n;
   }
 }
+
+
+// Solution 3
+public class Solution {
+  public int missing(int[] array) {
+    int n = array.length + 1;
+    HashSet<Integer> set = new HashSet<>();
+
+    for (int i : array) {
+      set.add(i);
+    }
+
+    for (int i = 1; i < n; ++i) {
+      if (!set.contains(i)) {
+        return i;
+      }
+    }
+
+    return n;
+  }
+}
