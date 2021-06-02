93021bd479ed434948cfac2c30a52837f9149c89
Ruiyue Wang <phynew@hotmail.com>
2021-06-02 17:39:46 -0400

    Create 652. Common Numbers Of Two Sorted Arrays(Array version).java

---
+++ 652. Common Numbers Of Two Sorted Arrays(Array version).java
@@ -0,0 +1,34 @@
+Easy
+Find all numbers that appear in both of two sorted arrays (the two arrays are all sorted in ascending order).
+
+Assumptions
+
+In each of the two sorted arrays, there could be duplicate numbers.
+Both two arrays are not null.
+Examples
+
+A = {1, 1, 2, 2, 3}, B = {1, 1, 2, 5, 6}, common numbers are [1, 1, 2]
+
+public class Solution {
+  public List<Integer> common(int[] A, int[] B) {
+    List<Integer> common = new ArrayList<>();
+    int i = 0;
+    int j = 0;
+
+    while (i != A.length && j != B.length) {
+      if (A[i] == B[j]) {
+        common.add(A[i]);
+        ++i;
+        ++j;
+      }
+      else if (A[i] < B[j]) {
+        ++i;
+      }
+      else {
+        ++j;
+      }
+    }
+
+    return common;
+  }
+}
