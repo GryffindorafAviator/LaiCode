b00e12f441166b9b7f739581bf071a378eab34dc
Ruiyue Wang <phynew@hotmail.com>
2021-06-02 00:29:26 -0400

    Update 67. Top K Frequent Words.java

--- 67. Top K Frequent Words.java
+++ 67. Top K Frequent Words.java
@@ -1,18 +1,18 @@
-Medium
-Given a composition with different kinds of words, return a list of the top K most frequent words in the composition.
+// Medium
+// Given a composition with different kinds of words, return a list of the top K most frequent words in the composition.
 
-Assumptions
+// Assumptions
 
-the composition is not null and is not guaranteed to be sorted
-K >= 1 and K could be larger than the number of distinct words in the composition, in this case, just return all the distinct words
-Return
+// the composition is not null and is not guaranteed to be sorted
+// K >= 1 and K could be larger than the number of distinct words in the composition, in this case, just return all the distinct words
+// Return
 
-a list of words ordered from most frequent one to least frequent one (the list could be of size K or smaller than K)
-Examples
+// a list of words ordered from most frequent one to least frequent one (the list could be of size K or smaller than K)
+// Examples
 
-Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 2 frequent words are [“b”, “c”]
-Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 4 frequent words are [“b”, “c”, "a", "d"]
-Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 5 frequent words are [“b”, “c”, "a", "d"]
+// Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 2 frequent words are [“b”, “c”]
+// Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 4 frequent words are [“b”, “c”, "a", "d"]
+// Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 5 frequent words are [“b”, “c”, "a", "d"]
 
 public class Solution {
   public String[] topKFrequent(String[] combo, int k) {
