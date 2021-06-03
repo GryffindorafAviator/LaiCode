46ddf2a243e0c876f730971784f200d0484e970c
Ruiyue Wang <phynew@hotmail.com>
2021-06-02 21:32:59 -0400

    Create 396. Reverse String.java

---
+++ 396. Reverse String.java
@@ -0,0 +1,32 @@
+Easy
+Reverse a given string.
+
+Assumptions
+
+The given string is not null.
+  
+public class Solution {
+  public String reverse(String input) {
+    if (input.length() == 0) {
+      return input;
+    }
+
+    char[] str = input.toCharArray();
+    int i = 0;
+    int j = str.length - 1;
+
+    while (i < j) {
+      swap(str, i, j);
+      ++i;
+      --j;
+    }
+
+    return new String(str, 0, str.length);
+  }
+
+  private void swap(char[] str, int i, int j) {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+  }
+}
