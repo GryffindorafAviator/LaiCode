e015f09ac1b76e1f85ca80b8eb7da096a7b0f61d
Ruiyue Wang <phynew@hotmail.com>
2021-06-05 17:15:31 -0400

    Create 397. Right Shift By N Characters.java

---
+++ 397. Right Shift By N Characters.java
@@ -0,0 +1,42 @@
+Easy
+Right shift a given string by n characters.
+
+Assumptions
+
+The given string is not null.
+n >= 0.
+Examples
+
+"abc", 4 -> "cab"
+  
+public class Solution {
+  public String rightShift(String input, int n) {
+    if (input == null || input.length() < 2 || n == 0) {
+      return input;
+    }
+
+    char[] str = input.toCharArray();
+    int len = str.length - 1;
+    int m = n % (len + 1);
+
+    reverse(str, 0, len);
+    reverse(str, 0, m - 1);
+    reverse(str, m, len);
+   
+    return new String(str);
+  }
+
+  private void reverse(char[] str, int start, int end) {
+    while (start < end) {
+      swap(str, start, end);
+      ++start;
+      --end;
+    }
+  }
+
+  private void swap(char[] str, int i, int j) {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+  }
+}
