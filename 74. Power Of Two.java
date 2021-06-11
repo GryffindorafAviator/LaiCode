851732223d4095b0bc15054c5fe8c65f00d4ac6a
Ruiyue Wang <phynew@hotmail.com>
2021-06-11 19:48:34 -0400

    Update 74. Power Of Two.java

--- 74. Power Of Two.java
+++ 74. Power Of Two.java
@@ -32,3 +32,18 @@ public class Solution {
     return (number > 0) && ((number & (number - 1)) == 0);
   }
 }
+
+// Solution 3
+public class Solution {
+  public boolean isPowerOfTwo(int number) {
+    if (number <= 0) {
+      return false;
+    }
+
+    while ((number & 1) == 0) {
+      number >>= 1;
+    }
+
+    return number == 1;
+  }
+}
