d1bd7c1228279e5f3f421cadd11b1551b8510d92
Ruiyue Wang <phynew@hotmail.com>
2021-06-11 19:35:56 -0400

    Update 75. Number Of Different Bits.java

--- 75. Number Of Different Bits.java
+++ 75. Number Of Different Bits.java
@@ -6,7 +6,6 @@
 // 5(“0101”) and 8(“1000”) has 3 different bits
 
 // Solution 1
-
 public class Solution {
   public int diffBits(int a, int b) {
     int cnt = 0;
@@ -15,6 +14,19 @@ public class Solution {
     for (int i = 0; i < 32; ++i) {
       cnt += (c & 1);
       c >>= 1;
+    }
+
+    return cnt;
+  }
+}
+
+// Solution 2
+public class Solution {
+  public int diffBits(int a, int b) {
+    int cnt = 0;
+
+    for (a ^= b; a != 0; a >>>= 1) {
+      cnt += (a & 1);
     }
 
     return cnt;
