ec319989c7a172982249694452143ebde80e1cda
Ruiyue Wang <phynew@hotmail.com>
2021-06-10 22:28:59 -0400

    Update 74. Power Of Two.java

--- 74. Power Of Two.java
+++ 74. Power Of Two.java
@@ -1,12 +1,12 @@
-Easy
-Determine if a given integer is power of 2.
+// Easy
+// Determine if a given integer is power of 2.
 
-Examples
+// Examples
 
-16 is power of 2 (2 ^ 4)
-3 is not
-0 is not
--1 is not
+// 16 is power of 2 (2 ^ 4)
+// 3 is not
+// 0 is not
+// -1 is not
 
 public class Solution {
   public boolean isPowerOfTwo(int number) {
