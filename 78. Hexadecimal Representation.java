6fb87d1841c95bc93081bf9d2d4123aba4a3f0f1
Ruiyue Wang <phynew@hotmail.com>
2021-06-12 03:24:45 -0400

    Update 78. Hexadecimal Representation.java

--- 78. Hexadecimal Representation.java
+++ 78. Hexadecimal Representation.java
@@ -1,12 +1,12 @@
-Easy
-Generate the hexadecimal representation for a given non-negative integer number as a string. The hex representation should start with "0x".
+// Easy
+// Generate the hexadecimal representation for a given non-negative integer number as a string. The hex representation should start with "0x".
 
-There should not be extra zeros on the left side.
+// There should not be extra zeros on the left side.
 
-Examples
+// Examples
 
-0's hex representation is "0x0"
-255's hex representation is "0xFF"
+// 0's hex representation is "0x0"
+// 255's hex representation is "0xFF"
   
 public class Solution {
   public String hex(int number) {
