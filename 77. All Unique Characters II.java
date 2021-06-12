c8e6d6efb7327ae6b99a00443cd67c5a50a352c7
Ruiyue Wang <phynew@hotmail.com>
2021-06-11 22:40:42 -0400

    Update 77. All Unique Characters II.java

--- 77. All Unique Characters II.java
+++ 77. All Unique Characters II.java
@@ -1,16 +1,16 @@
-Medium
-Determine if the characters of a given string are all unique.
+// Medium
+// Determine if the characters of a given string are all unique.
 
-Assumptions
+// Assumptions
 
-We are using ASCII charset, the value of valid characters are from 0 to 255
-The given string is not null
-Examples
+// We are using ASCII charset, the value of valid characters are from 0 to 255
+// The given string is not null
+// Examples
 
-all the characters in "abA+\8" are unique
-"abA+\a88" contains duplicate characters
+// all the characters in "abA+\8" are unique
+// "abA+\a88" contains duplicate characters
 
-Solution 1
+// Solution 1
 public class Solution {
   public boolean allUnique(String word) {
     Set<Character> set = new HashSet<>();
