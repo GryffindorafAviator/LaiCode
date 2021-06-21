d725af48a64570a1f1bf2b605df33215212808e2
Ruiyue Wang <phynew@hotmail.com>
2021-06-21 15:24:57 -0400

    Update 121. Spiral Order Traverse I.java

--- 121. Spiral Order Traverse I.java
+++ 121. Spiral Order Traverse I.java
@@ -1,21 +1,20 @@
-Medium
-Traverse an N * N 2D array in spiral order clock-wise starting from the top left corner. Return the list of traversal sequence.
+// Medium
+// Traverse an N * N 2D array in spiral order clock-wise starting from the top left corner. Return the list of traversal sequence.
 
-Assumptions
+// Assumptions
 
-The 2D array is not null and has size of N * N where N >= 0
-Examples
+// The 2D array is not null and has size of N * N where N >= 0
+// Examples
 
-{ {1,  2,  3},
+// { {1,  2,  3},
 
-  {4,  5,  6},
+//   {4,  5,  6},
 
-  {7,  8,  9} }
+//   {7,  8,  9} }
 
-the traversal sequence is [1, 2, 3, 6, 9, 8, 7, 4, 5]
+// the traversal sequence is [1, 2, 3, 6, 9, 8, 7, 4, 5]
 
-Solution
-
+// Solution
 public class Solution {
   public List<Integer> spiral(int[][] matrix) {
     List<Integer> ans = new ArrayList<>();
