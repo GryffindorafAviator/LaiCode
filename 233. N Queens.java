f06986cd2f2d89fea8874d57805f3af0fc2d815c
Ruiyue Wang <phynew@hotmail.com>
2021-06-21 19:02:51 -0400

    Update 233. N Queens.java

--- 233. N Queens.java
+++ 233. N Queens.java
@@ -1,22 +1,24 @@
-Medium
-Get all valid ways of putting N Queens on an N * N chessboard so that no two Queens threaten each other.
+// Medium
+// Get all valid ways of putting N Queens on an N * N chessboard so that no two Queens threaten each other.
 
-Assumptions
+// Assumptions
 
-N > 0
-Return
+// N > 0
+// Return
 
-A list of ways of putting the N Queens
-Each way is represented by a list of the Queen's y index for x indices of 0 to (N - 1)
-Example
+// A list of ways of putting the N Queens
+// Each way is represented by a list of the Queen's y index for x indices of 0 to (N - 1)
+// Example
 
-N = 4, there are two ways of putting 4 queens:
+// N = 4, there are two ways of putting 4 queens:
 
-[1, 3, 0, 2] --> the Queen on the first row is at y index 1, the Queen on the second row is at y index 3, 
-the Queen on the third row is at y index 0 and the Queen on the fourth row is at y index 2.
+// [1, 3, 0, 2] --> the Queen on the first row is at y index 1, the Queen on the second row is at y index 3, 
+// the Queen on the third row is at y index 0 and the Queen on the fourth row is at y index 2.
 
-[2, 0, 3, 1] --> the Queen on the first row is at y index 2, the Queen on the second row is at y index 0, 
-the Queen on the third row is at y index 3 and the Queen on the fourth row is at y index 1.
+// [2, 0, 3, 1] --> the Queen on the first row is at y index 2, the Queen on the second row is at y index 0, 
+// the Queen on the third row is at y index 3 and the Queen on the fourth row is at y index 1.
+  
+// Solution 1
   
 public class Solution {
   public List<List<Integer>> nqueens(int n) {
@@ -57,3 +59,5 @@ public class Solution {
     return true;
   }
 }
+
+// The time complexicity for vilidation is O(n).
