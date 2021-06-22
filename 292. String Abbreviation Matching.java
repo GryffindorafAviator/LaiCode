cfb53835f3527d602cd918dbb0cd6d43d088a0bc
Ruiyue Wang <phynew@hotmail.com>
2021-06-22 18:56:19 -0400

    Update 292. String Abbreviation Matching.java

--- 292. String Abbreviation Matching.java
+++ 292. String Abbreviation Matching.java
@@ -1,16 +1,16 @@
-Medium
-Word “book” can be abbreviated to 4, b3, b2k, etc. Given a string and an abbreviation, return if the string matches the abbreviation.
+// Medium
+// Word “book” can be abbreviated to 4, b3, b2k, etc. Given a string and an abbreviation, return if the string matches the abbreviation.
 
-Assumptions:
+// Assumptions:
 
-The original string only contains alphabetic characters.
-Both input and pattern are not null.
-Pattern would not contain invalid information like "a0a","0".
-Examples:
+// The original string only contains alphabetic characters.
+// Both input and pattern are not null.
+// Pattern would not contain invalid information like "a0a","0".
+// Examples:
 
-pattern “s11d” matches input “sophisticated” since “11” matches eleven chars “ophisticate”.
+// pattern “s11d” matches input “sophisticated” since “11” matches eleven chars “ophisticate”.
   
-Solution 1
+// Solution 1
 public class Solution {
   public boolean match(String input, String pattern) {
     if (input == null && pattern == null) {
