52263c5c1834af36df096aabb0bccca9058490ee
Ruiyue Wang <phynew@hotmail.com>
2021-06-21 23:26:01 -0400

    Update 35. Reverse Linked List In Pairs.java

--- 35. Reverse Linked List In Pairs.java
+++ 35. Reverse Linked List In Pairs.java
@@ -1,12 +1,12 @@
-Medium
-Reverse pairs of elements in a singly-linked list.
+// Medium
+// Reverse pairs of elements in a singly-linked list.
 
-Examples
+// Examples
 
-L = null, after reverse is null
-L = 1 -> null, after reverse is 1 -> null
-L = 1 -> 2 -> null, after reverse is 2 -> 1 -> null
-L = 1 -> 2 -> 3 -> null, after reverse is 2 -> 1 -> 3 -> null
+// L = null, after reverse is null
+// L = 1 -> null, after reverse is 1 -> null
+// L = 1 -> 2 -> null, after reverse is 2 -> 1 -> null
+// L = 1 -> 2 -> 3 -> null, after reverse is 2 -> 1 -> 3 -> null
 
 /**
  * class ListNode {
