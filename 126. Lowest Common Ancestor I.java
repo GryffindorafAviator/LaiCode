897a911165b202c4583f49a0c895734dfdb8242b
Ruiyue Wang <phynew@hotmail.com>
2021-06-23 18:59:39 -0400

    Update 126. Lowest Common Ancestor I.java

--- 126. Lowest Common Ancestor I.java
+++ 126. Lowest Common Ancestor I.java
@@ -1,27 +1,27 @@
-Medium
-Given two nodes in a binary tree, find their lowest common ancestor.
+// Medium
+// Given two nodes in a binary tree, find their lowest common ancestor.
 
-Assumptions
+// Assumptions
 
-There is no parent pointer for the nodes in the binary tree
+// There is no parent pointer for the nodes in the binary tree
 
-The given two nodes are guaranteed to be in the binary tree
+// The given two nodes are guaranteed to be in the binary tree
 
-Examples
+// Examples
 
-        5
+//         5
 
-      /   \
+//       /   \
 
-     9     12
+//      9     12
 
-   /  \      \
+//    /  \      \
 
-  2    3      14
+//   2    3      14
 
-The lowest common ancestor of 2 and 14 is 5
+// The lowest common ancestor of 2 and 14 is 5
 
-The lowest common ancestor of 2 and 9 is 9
+// The lowest common ancestor of 2 and 9 is 9
 
 /**
  * public class TreeNode {
