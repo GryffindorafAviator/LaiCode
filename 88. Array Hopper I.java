01cfc175d4226bb50245bc3ad9b8e58156845655
Ruiyue Wang <phynew@hotmail.com>
2021-06-25 02:26:28 -0400

    Update 88. Array Hopper I.java

--- 88. Array Hopper I.java
+++ 88. Array Hopper I.java
@@ -1,17 +1,17 @@
-Medium
-Given an array A of non-negative integers, 
-you are initially positioned at index 0 of the array. 
-A[i] means the maximum jump distance from that position (you can only jump towards the end of the array). 
-Determine if you are able to reach the last index.
+// Medium
+// Given an array A of non-negative integers, 
+// you are initially positioned at index 0 of the array. 
+// A[i] means the maximum jump distance from that position (you can only jump towards the end of the array). 
+// Determine if you are able to reach the last index.
 
-Assumptions
+// Assumptions
 
-The given array is not null and has length of at least 1.
-Examples
+// The given array is not null and has length of at least 1.
+// Examples
 
-{1, 3, 2, 0, 3}, we are able to reach the end of array(jump to index 1 then reach the end of the array)
+// {1, 3, 2, 0, 3}, we are able to reach the end of array(jump to index 1 then reach the end of the array)
 
-{2, 1, 1, 0, 2}, we are not able to reach the end of array
+// {2, 1, 1, 0, 2}, we are not able to reach the end of array
 
 public class Solution {
   public boolean canJump(int[] array) {
@@ -35,3 +35,5 @@ public class Solution {
     return dp[0];
   }
 }
+
+// TC: O(n), SC: O(n)
