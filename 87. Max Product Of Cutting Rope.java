0ecdc732e012ce2a58bb741d7b2da0ece52962fc
Ruiyue Wang <phynew@hotmail.com>
2021-06-24 23:16:15 -0400

    Update 87. Max Product Of Cutting Rope.java

--- 87. Max Product Of Cutting Rope.java
+++ 87. Max Product Of Cutting Rope.java
@@ -1,16 +1,16 @@
-Medium
-Given a rope with positive integer-length n, 
-how to cut the rope into m integer-length parts with length p[0], p[1], ...,p[m-1], 
-in order to get the maximal product of p[0]*p[1]* ... *p[m-1]? 
-m is determined by you and must be greater than 0 (at least one cut must be made). 
-Return the max product you can have.
+// Medium
+// Given a rope with positive integer-length n, 
+// how to cut the rope into m integer-length parts with length p[0], p[1], ...,p[m-1], 
+// in order to get the maximal product of p[0]*p[1]* ... *p[m-1]? 
+// m is determined by you and must be greater than 0 (at least one cut must be made). 
+// Return the max product you can have.
 
-Assumptions
+// Assumptions
 
-n >= 2
-Examples
+// n >= 2
+// Examples
 
-n = 12, the max product is 3 * 3 * 3 * 3 = 81(cut the rope into 4 pieces with length of each is 3).
+// n = 12, the max product is 3 * 3 * 3 * 3 = 81(cut the rope into 4 pieces with length of each is 3).
 
 public class Solution {
   public int maxProduct(int length) {
@@ -46,3 +46,5 @@ public class Solution {
     return dp[length - 1];
   }
 }
+
+// TC: O(n^2), SC: O(n)
