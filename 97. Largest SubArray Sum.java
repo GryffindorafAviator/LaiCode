777b4968c17ddf91ecaee7a0a7cf174e0f544352
Ruiyue Wang <phynew@hotmail.com>
2021-06-28 19:35:48 -0400

    Update 97. Largest SubArray Sum.java

--- 97. Largest SubArray Sum.java
+++ 97. Largest SubArray Sum.java
@@ -1,15 +1,15 @@
-Medium
-Given an unsorted integer array, find the subarray that has the greatest sum. 
-Return the sum.
+// Medium
+// Given an unsorted integer array, find the subarray that has the greatest sum. 
+// Return the sum.
 
-Assumptions
+// Assumptions
 
-The given array is not null and has length of at least 1.
-Examples
+// The given array is not null and has length of at least 1.
+// Examples
 
-{2, -1, 4, -2, 1}, the largest subarray sum is 2 + (-1) + 4 = 5
+// {2, -1, 4, -2, 1}, the largest subarray sum is 2 + (-1) + 4 = 5
 
-{-2, -1, -3}, the largest subarray sum is -1
+// {-2, -1, -3}, the largest subarray sum is -1
   
 public class Solution {
   public int largestSum(int[] array) {
@@ -32,3 +32,5 @@ public class Solution {
     return globalMax;
   }
 }
+
+// TC: O(n), SC: O(1)
