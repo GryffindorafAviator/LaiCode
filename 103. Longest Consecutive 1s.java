3462651b4e05ed08702ae4a148ed3f147cb0b007
Ruiyue Wang <phynew@hotmail.com>
2021-06-28 21:12:14 -0400

    Create 103. Longest Consecutive 1s.java

---
+++ 103. Longest Consecutive 1s.java
@@ -0,0 +1,32 @@
+Easy
+Given an array containing only 0s and 1s, 
+find the length of the longest subarray of consecutive 1s.
+
+Assumptions
+
+The given array is not null
+Examples
+
+{0, 1, 0, 1, 1, 1, 0}, the longest consecutive 1s is 3.
+  
+public class Solution {
+  public int longest(int[] array) {
+    int glbMax = 0;
+    int cnt = 0;
+
+    for (int i = 0; i < array.length; ++i) {
+      if (array[i] == 1) {
+        ++cnt;
+
+        if (cnt > glbMax) {
+          glbMax = cnt;
+        }
+      }
+      else {
+        cnt = 0;
+      }
+    }
+
+    return glbMax;
+  }
+}
