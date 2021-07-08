0b38756f7211955895303e5ccef50dbb345365a6
Ruiyue Wang <phynew@hotmail.com>
2021-07-07 20:43:13 -0400

    Create 368. Lowest Common Ancestor Binary Search Tree I.java

---
+++ 368. Lowest Common Ancestor Binary Search Tree I.java
@@ -0,0 +1,58 @@
+// Easy
+// Given two keys in a binary search tree, 
+// find their lowest common ancestor.
+
+// Assumptions
+
+// There is no parent pointer for the nodes in the binary search tree
+
+// There are no duplicate keys in the binary search tree
+
+// The given two nodes are guaranteed to be in the binary search tree
+
+// Examples
+
+//         5
+
+//       /   \
+
+//      2     12
+
+//    /  \      \
+
+//   1    3      14
+
+// The lowest common ancestor of 1 and 14 is 5
+
+// The lowest common ancestor of 1 and 3 is 2
+
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+
+public class Solution {
+  public TreeNode lca(TreeNode root, int p, int q) {
+    if (root == null || root.key == p || root.key == q) {
+      return root;
+    }
+
+    TreeNode left = lca(root.left, p, q);
+    TreeNode right = lca(root.right, p, q);
+
+    if (left != null && right != null) {
+      return root;
+    }
+    else {
+      return left == null ? right : left;
+    }
+  }
+}
+
+// TC: O(n), SC: O(1).
