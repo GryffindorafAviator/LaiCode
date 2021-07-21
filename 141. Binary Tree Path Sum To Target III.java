d3a413c9122283b52633c36bd104200a3846ef28
Ruiyue Wang <phynew@hotmail.com>
2021-07-20 21:12:37 -0400

    Update 141. Binary Tree Path Sum To Target III.java

--- 141. Binary Tree Path Sum To Target III.java
+++ 141. Binary Tree Path Sum To Target III.java
@@ -1,49 +1,49 @@
-Medium
-Given a binary tree in which each node contains an integer number. 
-Determine if there exists a path (the path can only be from one node to itself or to any of its descendants), 
-the sum of the numbers on the path is the given target number.
+// Medium
+// Given a binary tree in which each node contains an integer number. 
+// Determine if there exists a path (the path can only be from one node to itself or to any of its descendants), 
+// the sum of the numbers on the path is the given target number.
 
-Examples
+// Examples
 
-    5
+//     5
 
-  /    \
+//   /    \
 
-2      11
+// 2      11
 
-     /    \
+//      /    \
 
-    6     14
+//     6     14
 
-  /
+//   /
 
- 3
+//  3
 
-If target = 17, There exists a path 11 + 6, the sum of the path is target.
+// If target = 17, There exists a path 11 + 6, the sum of the path is target.
 
-If target = 20, There exists a path 11 + 6 + 3, the sum of the path is target.
+// If target = 20, There exists a path 11 + 6 + 3, the sum of the path is target.
 
-If target = 10, There does not exist any paths sum of which is target.
+// If target = 10, There does not exist any paths sum of which is target.
 
-If target = 11, There exists a path only containing the node 11.
+// If target = 11, There exists a path only containing the node 11.
 
-How is the binary tree represented?
+// How is the binary tree represented?
 
-We use the level order traversal sequence with a special symbol "#" denoting the null node.
+// We use the level order traversal sequence with a special symbol "#" denoting the null node.
 
-For Example:
+// For Example:
 
-The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+// The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
 
-    1
+//     1
 
-  /   \
+//   /   \
 
- 2     3
+//  2     3
 
-      /
+//       /
 
-    4
+//     4
 
 // Solution 1
 /**
@@ -93,5 +93,53 @@ public class Solution {
     return false;
   }
 }
-
 // TC: O(n^2), SC: O(height)
+
+// Solution 2
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean exist(TreeNode root, int target) {
+    if (root == null) {
+      return false;
+    }
+
+    Set<Integer> prefixSum = new HashSet<>();
+    prefixSum.add(0);
+
+    return helperDFS(root, target, prefixSum, 0);
+  }
+
+  private boolean helperDFS(TreeNode root, int target, Set<Integer> prefixSum, int preSum) {
+    preSum += root.key;
+
+    if (prefixSum.contains(preSum - target)) {
+      return true;
+    }
+
+    boolean needRemove = prefixSum.add(preSum);
+    
+    if (root.left != null && helperDFS(root.left, target, prefixSum, preSum)) {
+      return true;
+    }
+
+    if (root.right != null && helperDFS(root.right, target, prefixSum, preSum)) {
+      return true;
+    }
+
+    if (needRemove) {
+      prefixSum.remove(preSum);
+    }
+
+    return false;
+  }
+}
+// TC: O(n), SC: O(n)
