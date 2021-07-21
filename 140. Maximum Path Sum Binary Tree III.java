3e1a3f1c79cfa752722819943b9e56ee9ec615fe
Ruiyue Wang <phynew@hotmail.com>
2021-07-20 22:54:56 -0400

    Create 140. Maximum Path Sum Binary Tree III.java

---
+++ 140. Maximum Path Sum Binary Tree III.java
@@ -0,0 +1,99 @@
+// Medium
+// Given a binary tree in which each node contains an integer number. 
+// Find the maximum possible subpath sum(both the starting and ending node of the subpath should be on the same path from root to one of the leaf nodes, 
+// and the subpath is allowed to contain only one node).
+
+// Assumptions
+
+// The root of given binary tree is not null
+// Examples
+
+//    -5
+
+//   /    \
+
+// 2      11
+
+//      /    \
+
+//     6     14
+
+//            /
+
+//         -3
+
+// The maximum path sum is 11 + 14 = 25
+
+// How is the binary tree represented?
+
+// We use the level order traversal sequence with a special symbol "#" denoting the null node.
+
+// For Example:
+
+// The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+//     1
+
+//   /   \
+
+//  2     3
+
+//       /
+
+//     4
+
+// Solution 1
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public int maxPathSum(TreeNode root) {
+    Set<Integer> prefixSum = new HashSet<>();
+    int[] max = new int[]{Integer.MIN_VALUE};
+
+    prefixSum.add(0);
+
+    helperMax(root, prefixSum, 0, max);
+
+    return max[0];
+  }
+
+  private void helperMax(TreeNode root, Set<Integer> prefixSum, int preSum, int[] max) {
+    preSum += root.key;
+
+    Iterator numSet = prefixSum.iterator();
+
+    while (numSet.hasNext()) {
+      int temp = preSum - (int) numSet.next();
+
+      if (temp > max[0]) {
+        max[0] = temp;
+      }
+    }
+
+    boolean needRemove = prefixSum.add(preSum);
+
+    if (root.left != null) {
+      helperMax(root.left, prefixSum, preSum, max);
+    }
+
+    if (root.right != null) {
+      helperMax(root.right, prefixSum, preSum, max);
+    }
+
+    if (needRemove) {
+      prefixSum.remove(preSum);
+    }
+
+    return;
+  }
+}
+// TC: O(n^2), SC: O(n)
+
