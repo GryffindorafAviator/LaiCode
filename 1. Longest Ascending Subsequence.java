57762f8b2684b0fec71da28cc9cecbf7097b8d1e
Ruiyue Wang <phynew@hotmail.com>
2021-10-25 23:40:12 -0400

    Update 1. Longest Ascending Subsequence.java

--- 1. Longest Ascending Subsequence.java
+++ 1. Longest Ascending Subsequence.java
@@ -38,3 +38,52 @@ public class Solution {
 }
 
 // TC: O(n^2); SC: O(n)
+
+// Solution 2
+public class Solution {
+  public int longest(int[] array) {
+    if (array == null || array.length == 0) {
+      return 0;
+    }
+
+    int len = array.length;
+    int result = 1;
+    int[] smlRec = new int[len + 1];
+    smlRec[1] = array[0];
+
+    for (int i = 1; i < len; ++i) {
+      int distance = find(smlRec, 1, result, array[i]);
+
+      if (distance == result) {
+        ++result;
+      }
+
+      smlRec[distance + 1] = array[i];
+    }
+
+    return result;
+  }
+
+  private int find(int[] smlRec, int sh, int lo, int target) {
+    while (sh < lo - 1) {
+      int mid = sh + (lo - sh) / 2;
+        
+      if (smlRec[mid] >= target) {
+        lo = mid - 1;
+      }
+      else {
+        sh = mid;
+      }
+    }
+
+    for (int i = lo; i >= sh; --i) {
+        if (smlRec[i] < target) {
+          return i;
+        }
+      }
+
+    return 0;
+  }
+}
+
+// TC: O(nlogn); SC: O(n)
