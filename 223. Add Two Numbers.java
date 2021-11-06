6eae0d23cc427b2314ab1c94e59043569f933143
Ruiyue Wang <phynew@hotmail.com>
2021-11-06 17:02:36 -0400

    Update 223. Add Two Numbers.java

--- 223. Add Two Numbers.java
+++ 223. Add Two Numbers.java
@@ -71,3 +71,42 @@ public class Solution {
 }
 
 // TC: O(n); SC: O(1)
+
+// Solution 2
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummy = new ListNode(0);
+    ListNode cur = dummy;
+    int val = 0;
+
+    while (l1 != null || l2 != null || val != 0) {
+      if (l1 != null) {
+        val += l1.value;
+        l1 = l1.next;
+      }
+
+      if (l2 != null) {
+        val += l2.value;
+        l2 = l2.next;
+      }
+
+      cur.next = new ListNode(val % 10);
+      val /= 10;
+      cur = cur.next;
+    }
+
+    return dummy.next;
+  }
+}
+
+// TC: O(n); SC: O(1)
