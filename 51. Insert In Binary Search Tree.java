18b8da9ac4e094df35fac9e3ccbaea11a6ef5c2b
Ruiyue Wang <phynew@hotmail.com>
2021-11-06 22:08:26 -0400

    Update 51. Insert In Binary Search Tree.java

--- 51. Insert In Binary Search Tree.java
+++ 51. Insert In Binary Search Tree.java
@@ -43,7 +43,7 @@
 
 //  1     4   6    11
 
-// Solution
+// Solution 1, iterative
 /**
  * public class TreeNode {
  *   public int key;
@@ -85,3 +85,34 @@ public class Solution {
 
 // TC: avg, O(logn); worst, O(n)
 // SC: O(1)
+
+// Solution 2, recursive
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode insert(TreeNode root, int key) {
+    if (root == null) {
+      return new TreeNode(key);
+    }
+
+    if (key < root.key) {
+      root.left = insert(root.left, key);
+    }
+    else if (key > root.key) {
+      root.right = insert(root.right, key);
+    }
+
+    return root;
+  }
+}
+
+// TC: avg, O(logn); worst, O(n)
+// SC: avg, O(logn); worst, O(n)
