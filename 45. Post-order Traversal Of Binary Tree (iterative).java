e94d14a836ba1a3287e305617f5bc17067c017de
Ruiyue Wang <phynew@hotmail.com>
2021-11-08 22:29:13 -0500

    Create 45. Post-order Traversal Of Binary Tree (iterative).java

---
+++ 45. Post-order Traversal Of Binary Tree (iterative).java
@@ -0,0 +1,95 @@
+// Hard
+// Implement an iterative, 
+// post-order traversal of a given binary tree, 
+// return the list of keys of each node in the tree as it is post-order traversed.
+
+// Examples
+
+//         5
+
+//       /    \
+
+//     3        8
+
+//   /   \        \
+
+// 1      4        11
+
+// Post-order traversal is [1, 4, 3, 11, 8, 5]
+
+// Corner Cases
+// What if the given binary tree is null? 
+// Return an empty list in this case.
+// How is the binary tree represented?
+
+// We use the level order traversal sequence with a special symbol "#" denoting the null node.
+
+// For Example:
+// The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+//     1
+
+//   /   \
+
+//  2     3
+
+//       /
+
+//     4
+
+// Solution 1
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public List<Integer> postOrder(TreeNode root) {
+    List<Integer> ans = new ArrayList<>();
+
+    if (root == null) {
+      return ans;
+    }
+
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode prev = null;
+    
+    stack.offerFirst(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode curr = stack.peekFirst();
+
+      if (prev == null || curr == prev.left || curr == prev.right) {
+        if (curr.left != null) {
+          stack.offerFirst(curr.left);
+        }
+        else if (curr.right != null) {
+          stack.offerFirst(curr.right);
+        }
+        else {
+          stack.pollFirst();
+          ans.add(curr.key);
+        }  
+      }
+      else if (prev == curr.left && curr.right != null) {
+        stack.offerFirst(curr.right);
+      }
+      else {
+        curr = stack.pollFirst();
+        ans.add(curr.key);
+      }
+
+      prev = curr;
+    }
+
+    return ans;
+  }
+}
+
+// TC: O(n); 
+// SC: avg, O(logn); worst, O(n)
