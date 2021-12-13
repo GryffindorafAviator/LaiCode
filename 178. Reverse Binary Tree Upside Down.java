9050d81fdd320451f2210683fc6ed8dc0d0a76d6
Ruiyue Wang <phynew@hotmail.com>
2021-12-12 22:11:38 -0500

    Create 178. Reverse Binary Tree Upside Down.java

---
+++ 178. Reverse Binary Tree Upside Down.java
@@ -0,0 +1,57 @@
+// Medium
+// Given a binary tree where all the right nodes are leaf nodes, 
+// flip it upside down and turn it into a tree with left leaf nodes as the root.
+
+// Examples
+
+//         1
+
+//       /    \
+
+//     2        5
+
+//   /   \
+
+// 3      4
+
+// is reversed to
+
+//         3
+
+//       /    \
+
+//     2        4
+
+//   /   \
+
+// 1      5
+  
+// Solution 1, Recursion
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode reverse(TreeNode root) {
+    if (root == null || root.left == null) {
+      return root;
+    }
+
+    TreeNode newHead = reverse(root.left);
+
+    root.left.left = root;
+    root.left.right = root.right;
+    root.left = null;
+    root.right = null;
+
+    return newHead;
+  }
+}
+
+// TC: O(n); SC: O(n)
