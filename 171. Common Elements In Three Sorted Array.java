70b6de69629177c39fe9f684fcb7259d55331dab
Ruiyue Wang <phynew@hotmail.com>
2022-01-07 18:46:08 -0500

    Update 171. Common Elements In Three Sorted Array.java

--- 171. Common Elements In Three Sorted Array.java
+++ 171. Common Elements In Three Sorted Array.java
@@ -9,7 +9,7 @@
 // A = {1, 2, 2, 3}, B = {2, 2, 3, 5}, C = {2, 2, 4}, 
 // the common elements are [2, 2]
 
-// Solution
+// Solution 1
 public class Solution {
   public List<Integer> common(int[] a, int[] b, int[] c) {
     List<Integer> ans = new ArrayList<>();
@@ -52,6 +52,46 @@ public class Solution {
       }
       else {
         bi++;
+        ci++;
+      }
+    }
+
+    return ans;
+  }
+}
+// TC: O(a.len + b.len + c.len); SC: O(1)
+
+// Solution 2
+public class Solution {
+  public List<Integer> common(int[] a, int[] b, int[] c) {
+    List<Integer> ans = new ArrayList<>();
+
+    if (a == null || b == null || c == null) {
+      return ans;
+    }
+
+    if (a.length == 0 || b.length == 0 || c.length == 0) {
+      return ans;
+    }
+
+    int ai = 0;
+    int bi = 0;
+    int ci = 0;
+
+    while (ai < a.length && bi < b.length && ci < c.length) {
+      if (a[ai] == b[bi] && b[bi] == c[ci]) {
+        ans.add(a[ai]);
+        ai++;
+        bi++;
+        ci++;
+      }
+      else if (a[ai] <= b[bi] && a[ai] <= c[ci]) {
+        ai++;
+      }
+      else if (b[bi] <= a[ai] && b[bi] <= c[ci]) {
+        bi++;
+      }
+      else {
         ci++;
       }
     }
