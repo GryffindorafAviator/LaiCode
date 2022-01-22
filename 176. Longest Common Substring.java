24ea44e3857306003cf7d921b9b513bd0b81a2da
Ruiyue Wang <phynew@hotmail.com>
2022-01-21 23:05:11 -0500

    Update 176. Longest Common Substring.java

--- 176. Longest Common Substring.java
+++ 176. Longest Common Substring.java
@@ -45,3 +45,35 @@ public class Solution {
   }
 }
 // TC: O(n^2); SC: O(n^2)
+
+// Solution 2
+public class Solution {
+  public int longest(String source, String target) {
+    if (source == null || target == null) {
+      return 0;
+    }
+
+    int sLen = source.length();
+    int tLen = target.length();
+
+    if (sLen == 0 || tLen == 0) {
+      return 0;
+    }
+
+    int[][] same = new int[sLen + 1][tLen + 1];
+
+    for (int i = 1; i <= sLen; i++) {
+      for (int j = 1; j <= tLen; j++) {
+        if (source.charAt(i - 1) == target.charAt(j - 1)) {
+          same[i][j] = same[i - 1][j - 1] + 1;
+        }
+        else {
+          same[i][j] = Math.max(same[i - 1][j], same[i][j - 1]);
+        }
+      }
+    }
+
+    return same[sLen][tLen];
+  }
+}
+// TC: O(n^2); SC: O(n^2)
