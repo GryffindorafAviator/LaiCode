ac312de407f1530540ff6ccaf422f7d64552452e
Ruiyue Wang <phynew@hotmail.com>
2022-04-05 23:00:15 -0400

    Update 89. Array Hopper II.java

--- 89. Array Hopper II.java
+++ 89. Array Hopper II.java
@@ -1,19 +1,22 @@
 // Medium
-// Given an array A of non-negative integers, you are initially positioned at index 0 of the array. 
+// Given an array A of non-negative integers, 
+// you are initially positioned at index 0 of the array. 
 // A[i] means the maximum jump distance from index i (you can only jump towards the end of the array). 
 // Determine the minimum number of jumps you need to reach the end of array. 
 // If you can not reach the end of the array, 
 // return -1.
 
 // Assumptions
-
 // The given array is not null and has length of at least 1.
+
 // Examples
+// {3, 3, 1, 0, 4}, 
+// the minimum jumps needed is 2 (jump to index 1 then to the end of array)
 
-// {3, 3, 1, 0, 4}, the minimum jumps needed is 2 (jump to index 1 then to the end of array)
+// {2, 1, 1, 0, 2}, 
+// you are not able to reach the end of array, return -1 in this case.
 
-// {2, 1, 1, 0, 2}, you are not able to reach the end of array, return -1 in this case.
-  
+// Solution
 public class Solution {
   public int minJump(int[] array) {
     int[] steps = new int[array.length];
