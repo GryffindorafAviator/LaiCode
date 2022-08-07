100553bcd0beb48fc77bd92be8a04367263245cf
Ruiyue Wang <phynew@hotmail.com>
2022-08-06 20:57:48 -0400

    Create 49. Identical Binary Tree.java

---
+++ 49. Identical Binary Tree.java
@@ -0,0 +1,70 @@
+Easy
+Check if two given binary trees are identical. 
+Identical means the equal valued keys are at the same position in the two binary trees.
+
+Examples
+        5
+
+      /    \
+
+    3        8
+
+and
+
+        5
+
+      /    \
+
+    3        8
+
+are identical trees.
+
+How is the binary tree represented?
+
+We use the level order traversal sequence with a special symbol "#" denoting the null node.
+
+For Example:
+The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4
+      
+// Solution
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean isIdentical(TreeNode one, TreeNode two) {
+    if (one == null) {
+      return two == null ? true : false;
+    }
+
+    if (two == null) {
+      return false;
+    }
+
+    if (one.key != two.key) {
+      return false;
+    }
+
+    boolean left = isIdentical(one.left, two.left);
+    boolean right = isIdentical(one.right, two.right);
+    
+    return left && right;
+  }
+}
+// TC: O(n); SC: O(lgn)
