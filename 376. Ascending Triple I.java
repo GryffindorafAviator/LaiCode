bced44396cbad77fe1fb2307f7714fcb38cfe2e1
Ruiyue Wang <phynew@hotmail.com>
2022-08-30 15:54:18 -0400

    Create 376. Ascending Triple I.java

---
+++ 376. Ascending Triple I.java
@@ -0,0 +1,36 @@
+// Easy
+// Determine if the given integer array has three indices such that i < j < k and a[i] < a[j] < a[k].
+
+// Assumptions:
+// The given array is not null.
+  
+// Examples:
+// {1, 5, 2, 4}, return true since i = 0, j = 2, k = 3
+// {4, 3, 2, 1}, return false
+  
+// Solution
+public class Solution {
+  public boolean existIJK(int[] array) {
+    int min1 = Integer.MAX_VALUE;
+    int min2 = Integer.MAX_VALUE;
+
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == min1 || array[i] == min2) {
+        continue;
+      }
+
+      if (array[i] < min1) {
+        min1 = array[i];
+      }
+      else if (array[i] < min2) {
+        min2 = array[i];
+      }
+      else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
+// TC: O(n); SC: O(1)
